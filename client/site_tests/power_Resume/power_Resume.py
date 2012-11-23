5fc13839da93db5533d5db81d88b08d799a21382
Ricky Liang <jcliang@chromium.org>
2012-11-23 14:44:36 +0800

    Revert "BUTTERFLY_FACTORY: [autotest] power_Resume: stop tlsdated"
    There's no tlsdated job on Butterfly.
    
    This reverts commit 6095700ecc46ef3cd07db6254d810e7de6f6bf3b.
    
    BUG=None
    TEST=Manually test on my DUT
    
    Change-Id: I0005b2848a2654dd74014e14d9d388cfd89a08e7
    Reviewed-on: https://gerrit.chromium.org/gerrit/38584
    Reviewed-by: Ricky Liang <jcliang@chromium.org>
    Tested-by: Ricky Liang <jcliang@chromium.org>

--- client/site_tests/power_Resume/power_Resume.py
+++ client/site_tests/power_Resume/power_Resume.py
@@ -264,7 +264,6 @@ class power_Resume(test.test):
 
 
     def run_once(self, max_devs_returned=10):
-        utils.system('initctl stop tlsdated')
         # Check hwclock is working
         CheckHwclock()
 
@@ -373,4 +372,3 @@ class power_Resume(test.test):
 
     def cleanup(self):
         self._disable_pm_print_times()
-        utils.system('initctl start tlsdated')
