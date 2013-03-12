5ad28323d8351379b147a7a24d319132179ab544
Ricky Liang <jcliang@chromium.org>
2013-03-13 01:17:40 +0800

    graphics_GLMark2: Fix GLMark2 so that it runs on Exynos platform.
    Tweaked the build procedure so that it builds glmark2 with glesv2 on
    Exynos platform. Also changed graphics_GLMark2.py to run binary
    "glmark2-es2" if "glmark2" is not available.
    
    BUG=chrome-os-partner:17642, chromium-os:38184, chromium-os:39541
    TEST=Manually tested:
    cros_workon-daisy start autotest-deps-glmark2
    emerge-daisy autotest-deps-glmark2
    synced the binary built to daisy, and make sure it ran properly.
    
    Change-Id: I7c3cdf56b083c29d154633904e46f5c9b0ca84bc
    Reviewed-on: https://gerrit.chromium.org/gerrit/45682
    Commit-Queue: Ricky Liang <jcliang@chromium.org>
    Reviewed-by: Ricky Liang <jcliang@chromium.org>
    Tested-by: Ricky Liang <jcliang@chromium.org>

--- client/deps/glmark2/glmark2.py
+++ client/deps/glmark2/glmark2.py
@@ -10,19 +10,30 @@ from autotest_lib.client.bin import utils
 
 
 PREFIX_DIR = '/usr/local/autotest/deps/glmark2'
+PATCH_FILE = 'exynos-glmark2-2012.03.patch'
 version = 1
 
 
 def setup(tarball, topdir):
+    use_flags = os.environ.get('USE', '').split()
     srcdir = os.path.join(topdir, 'src')
     utils.extract_tarball_to_dir(tarball, srcdir)
     os.chdir(srcdir)
+    if 'exynos' in use_flags:
+        # Apply patch so that glmark2-es2 runs on Exynos platform without
+        # glitch.
+        patch_file = os.path.join(topdir, PATCH_FILE)
+        utils.system('patch -p1 -i %s' % patch_file)
+    if 'opengles' in use_flags:
+        gl_target = '--enable-glesv2'
+    else:
+        gl_target = '--enable-gl'
     # glmark2 does not have any runtime option to specify its data dir, so we
     # have to set the prefix dir to where it's being run on target machine.
     # And we can only install glmark2 to inside the build sandbox (destdir), so
     # we have to do additional work to move the installed files to the correct
     # path and it will get installed on target machine correctly.
-    utils.system('./waf configure --enable-gl --prefix=%s' % PREFIX_DIR)
+    utils.system('./waf configure %s --prefix=%s' % (gl_target, PREFIX_DIR))
     utils.system('./waf')
     utils.system('./waf install --destdir=%s' % topdir)
     utils.system('mv %s/* %s' % (topdir + PREFIX_DIR, topdir))
--- client/site_tests/graphics_GLMark2/graphics_GLMark2.py
+++ client/site_tests/graphics_GLMark2/graphics_GLMark2.py
@@ -29,6 +29,9 @@ class graphics_GLMark2(test.test):
         dep_dir = os.path.join(self.autodir, 'deps', dep)
         self.job.install_pkg(dep, 'dep', dep_dir)
         glmark2 = os.path.join(dep_dir, 'bin/glmark2')
+        if not os.path.exists(glmark2):
+          glmark2 = os.path.join(dep_dir, 'bin/glmark2-es2')
+
         options = []
         options.append('--size %s' % size)
         if validation_mode:
