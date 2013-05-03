6769fdf429a31813ef690818544d4eb069dc1e6f
henryhsu <henryhsu@chromium.org>
2013-05-03 15:08:01 +0800

    Auto detect ethernet and hide FA utility
    BUG=chrome-os-partner:18533
    TEST=Manual test run AudioQuality
    
    Change-Id: I51b839e8f617637b9023ac15b790eb0e87e6f823
    Reviewed-on: https://gerrit.chromium.org/gerrit/50027
    Reviewed-by: Hsinyu Chao <hychao@chromium.org>
    Commit-Queue: Heng-ruey Hsu <henryhsu@chromium.org>
    Tested-by: Heng-ruey Hsu <henryhsu@chromium.org>

--- client/site_tests/factory_AudioQuality/static/factory_AudioQuality.js
+++ client/site_tests/factory_AudioQuality/static/factory_AudioQuality.js
@@ -1,11 +1,18 @@
+/*
 window.onkeydown = function(event) {
   if (event.keyCode == 83) {
     test.sendTestEvent("init_audio_server",{});
-    window.onkeydown = null;
   }
+}
+*/
+
+window.onload = function(event) {
+  test.sendTestEvent("init_audio_server",{});
 }
 
 var active = 'loop_0';
+var display_fa_utility = false;
+var count = 3, count_timer = null, count_msg = '';
 
 function setMessage(msg) {
   document.getElementById("message").innerHTML = msg;
@@ -23,4 +30,29 @@ function restore() {
     document.getElementById(active).checked = false;
   testCommand('loop_0');
   document.getElementById('loop_0').checked = true;
+}
+
+function toggleFAUtility() {
+  if (display_fa_utility) {
+    document.getElementById('fa-utility').style.display = 'none';
+    display_fa_utility = false;
+  } else {
+    document.getElementById('fa-utility').style.display = 'block';
+    display_fa_utility = true;
+  }
+}
+
+function countdown() {
+  setMessage(count_msg + ' ' + count);
+  count = count - 1;
+  if (count <= 0) {
+     clearInterval(count_timer);
+     test.sendTestEvent("init_audio_server",{});
+  }
+}
+
+function start_countdown(msg, num) {
+  count = num;
+  count_msg = msg;
+  count_timer = setInterval(countdown, 1000);
 }
