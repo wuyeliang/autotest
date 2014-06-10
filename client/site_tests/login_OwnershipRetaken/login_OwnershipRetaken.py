fc5f4ef4f60061b49dfa18263b4f8d6bca569da7
Chris Masone <cmasone@chromium.org>
2014-06-10 16:01:01 -0700

    Stop the UI instead of stopping the session in some tests.
    Some tests need to unmount and remove a cryptohome at the end of the test.
    Stopping the user's session and then unmounting the user's cryptohome
    can cause a race with ui.conf's session cleanup code (which ALSO wants to
    own unmounting the user's cryptohome). Find the tests that were manually
    unmounting during cleanup and make them stop or restart the UI instead.
    BUG=chromium:381034
    TEST=run these three tests.
    
    Change-Id: I2fae64fdfea8da569e5ab61691fddbe3d1157708
    Reviewed-on: https://chromium-review.googlesource.com/203320
    Reviewed-by: Darren Krahn <dkrahn@chromium.org>
    Tested-by: Chris Masone <cmasone@chromium.org>
    Commit-Queue: Chris Masone <cmasone@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/205990

--- client/site_tests/login_OwnershipRetaken/login_OwnershipRetaken.py
+++ client/site_tests/login_OwnershipRetaken/login_OwnershipRetaken.py
@@ -89,7 +89,7 @@ class login_OwnershipRetaken(test.test):
 
 
     def cleanup(self):
-        self._cryptohome_proxy.unmount(ownership.TESTUSER)
         if self._tempdir: self._tempdir.clean()
-        cros_ui.start(allow_fail=True)
+        cros_ui.restart()
+        self._cryptohome_proxy.remove(ownership.TESTUSER)
         super(login_OwnershipRetaken, self).cleanup()
