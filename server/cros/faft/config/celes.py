jingpad-bsp/android_external_autotest

Celes: Add FAFT config for Celes
BUG=chrome-os-partner:42787
TEST=None

Change-Id: Ib5f15f84514e5f6576e25be3941f62db108ba418
Signed-off-by: Anson Tseng <anson.tseng@intel.com>
Reviewed-on: https://chromium-review.googlesource.com/286971
Reviewed-by: Shawn N <shawnn@chromium.org>

server/cros/faft/config/celes.py
@@ -1,0 +1,11 @@
+# Copyright 2015 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""FAFT config setting overrides for Celes."""
+
+from autotest_lib.server.cros.faft.config import cyan
+
+class Values(cyan.Values):
+    """Inherit overrides from Cyan."""
+    pass

wuyeliang/autotest

(cherry picked from commit 5cd5bdf6dca5283b6dd8fe09b200c3ae84dc4fb3)
Reviewed-on: https://chromium-review.googlesource.com/316383
Reviewed-by: Bernie Thompson <bhthompson@chromium.org>

server/cros/faft/config/celes.py
@@ -1,0 +1,11 @@
+# Copyright 2015 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""FAFT config setting overrides for Celes."""
+
+from autotest_lib.server.cros.faft.config import strago
+
+class Values(strago.Values):
+    """Inherit overrides from Strago."""
+    pass