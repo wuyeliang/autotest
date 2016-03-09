jingpad-bsp/android_external_autotest

Ultima: Add FAFT config for ultima
BUG=chrome-os-partner:43821
TEST=None

Change-Id: Ic40989e53ef06259aa13ce4eb5902deb7c1985c0
Reviewed-on: https://chromium-review.googlesource.com/293098
Reviewed-by: Shawn N <shawnn@chromium.org>
Commit-Queue: Keith Tzeng <keith.tzeng@quantatw.com>
Tested-by: Keith Tzeng <keith.tzeng@quantatw.com>

server/cros/faft/config/ultima.py
@@ -1,0 +1,11 @@
+# Copyright 2015 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""FAFT config setting overrides for Ultima."""
+
+from autotest_lib.server.cros.faft.config import cyan
+
+class Values(cyan.Values):
+    """Inherit overrides from Cyan."""
+    pass

wuyeliang/autotest

BUG=chrome-os-partner:49607
(cherry picked from commit 059ba41ff293544e05740019ce0d7d19c6ee08e4)
Reviewed-on: https://chromium-review.googlesource.com/331955
Reviewed-by: Bernie Thompson <bhthompson@chromium.org>

server/cros/faft/config/ultima.py
@@ -1,0 +1,11 @@
+# Copyright 2015 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""FAFT config setting overrides for Ultima."""
+
+from autotest_lib.server.cros.faft.config import strago
+
+class Values(strago.Values):
+    """Inherit overrides from Strago."""
+    pass