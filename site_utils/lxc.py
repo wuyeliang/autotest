8efbf71787dae694b09aa1298218ca2d8bff5476
Dan Shi <dshi@google.com>
2016-08-31 21:47:34 -0700

    [autotest] Bug fix in container hostname
    CL 376221 introduced a bug in server-side packaging running in Moblab.
    In Moblab, the hostname of duts are IP address, and the Moblab functions as a
    dhcp server for the duts. For the container to have the same name as the dut
    it's testing will confuse the dhcp server.
    Also, the container's hostname must start with `test_` to allow DHCP server in
    Moblab to set a correct lease time (20min)
    
    This change modify the container name to be a string different from the dut's
    hostname. Also, replace dot in the hostname with understore to avoid dns
    problem.
    
    BUG=chromium:642783
    TEST=in a moblab, sudo python site_utils/lxc_functional_test.py
    
    Change-Id: I9378b4a23f079f54f130e779a723f33c79635dab
    Reviewed-on: https://chromium-review.googlesource.com/379613
    Commit-Ready: Dan Shi <dshi@google.com>
    Tested-by: Dan Shi <dshi@google.com>
    Reviewed-by: Dan Shi <dshi@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/403132
    Trybot-Ready: Dan Shi <dshi@google.com>
    Reviewed-by: Mohan Konduri <krk@chromium.org>
    Reviewed-by: Simran Basi <sbasi@chromium.org>

--- site_utils/lxc.py
+++ site_utils/lxc.py
@@ -106,6 +106,10 @@ CONTAINER_CREATE_METADB_TYPE = 'container_create'
 CONTAINER_CREATE_RETRY_METADB_TYPE = 'container_create_retry'
 CONTAINER_RUN_TEST_METADB_TYPE = 'container_run_test'
 
+# The container's hostname MUST start with `test_`. DHCP server in MobLab uses
+# that prefix to determine the lease time.
+CONTAINER_UTSNAME_FORMAT = 'test_%s'
+
 STATS_KEY = 'lxc.%s' % socket.gethostname().replace('.', '_')
 timer = autotest_stats.Timer(STATS_KEY)
 # Timer used inside container should not include the hostname, as that will
@@ -885,6 +889,20 @@ class ContainerBucket(object):
 
         # Create test container from the base container.
         container = self.create_from_base(name)
+
+        # Update the hostname of the test container to be `dut_name`.
+        # Some TradeFed tests use hostname in test results, which is used to
+        # group test results in dashboard. The default container name is set to
+        # be the name of the folder, which is unique (as it is composed of job
+        # id and timestamp. For better result view, the container's hostname is
+        # set to be a string containing the dut hostname.
+        if dut_name:
+            config_file = os.path.join(container.container_path, name, 'config')
+            lxc_utsname_setting = (
+                    'lxc.utsname = ' +
+                    CONTAINER_UTSNAME_FORMAT % dut_name.replace('.', '_'))
+            utils.run(APPEND_CMD_FMT % {'content': lxc_utsname_setting,
+                                        'file': config_file})
 
         # Deploy server side package
         usr_local_path = os.path.join(container.rootfs, 'usr', 'local')
