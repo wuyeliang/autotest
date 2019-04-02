8567f8565f8313904a822cc3758ccd0b9c8277a0
Kevin Shelton <kmshelton@chromium.org>
2019-04-02 15:41:06 -0700

    firmware_ECLidShutdown: align to GBB flag behavior
    This change reworks this test to align to the expected behavior of
    GBB_FLAG_DISABLE_LID_SHUTDOWN.
    
    BUG=b:35584893
    TEST=Ran against a samus.
    
    Change-Id: I35448e0a246742ccf5814ce8309b073679fec68a
    Reviewed-on: https://chromium-review.googlesource.com/1551961
    Commit-Ready: Kevin Shelton <kmshelton@chromium.org>
    Tested-by: Kevin Shelton <kmshelton@chromium.org>
    Reviewed-by: Dana Goyette <dgoyette@chromium.org>

--- server/cros/faft/config/samus.py
+++ server/cros/faft/config/samus.py
@@ -14,7 +14,7 @@ class Values(object):
     ec_capability = ['adc_ectemp', 'battery', 'charging', 'keyboard',
                      'lid', 'x86', 'usb', 'peci', 'smart_usb_charge',
                      'usbpd_uart' ]
-    firmware_screen = 12
+    firmware_screen = 15
     spi_voltage = 'pp3300'
     servo_prog_state_delay = 10
     smm_store = False
