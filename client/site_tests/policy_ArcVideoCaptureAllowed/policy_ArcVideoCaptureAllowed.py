bace94bdf7d15207e4f6ed1118974f666cac7b5f
Derek Beckett <dbeckett@google.com>
2019-04-29 06:53:47 -0700

    [autotest] Fix ArcVideoCaptureAllowed
    Some fixes to reduce the failing of ArcVideoCaptureAllowed.
    
    This CL currently adds a long sleep. There are much more complicated
    ways to get around this, but for now this will test if this fix will
    work in the lab. It has been tested on both local/lab machines and looks
    OK.
    
    TEST=this
    BUG=None
    
    Change-Id: I9026551e437aa00efb6eb859235d8dd864d0dd83
    Reviewed-on: https://chromium-review.googlesource.com/1587482
    Commit-Ready: Derek Beckett <dbeckett@chromium.org>
    Tested-by: Derek Beckett <dbeckett@chromium.org>
    Reviewed-by: Ruben Zakarian <rzakarian@chromium.org>

--- client/site_tests/policy_ArcVideoCaptureAllowed/policy_ArcVideoCaptureAllowed.py
+++ client/site_tests/policy_ArcVideoCaptureAllowed/policy_ArcVideoCaptureAllowed.py
@@ -1,6 +1,7 @@
 # Copyright 2019 The Chromium OS Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
+import time
 
 from autotest_lib.client.common_lib import error
 from autotest_lib.client.common_lib import utils
@@ -24,6 +25,52 @@ class policy_ArcVideoCaptureAllowed(
     """
     version = 1
 
+    def _launch_Arc_Cam(self):
+        """Grant the Camera location permission, and launch the Camera app."""
+        utils.poll_for_condition(
+            lambda: self.did_cam_app_respond(),
+            exception=error.TestFail('Camera APP did not respond.'),
+            timeout=25,
+            sleep_interval=5,
+            desc='Wait for Camera to respond.')
+
+    def _cam_closed(self):
+        """Check if the camera got closed after it opened."""
+        try:
+            cam_status = arc.adb_shell(
+                "logcat -d | grep camera | grep Closing")
+        except error.CmdError:
+            return False
+        return cam_status
+
+    def _check_cam_status(self):
+        """Returns the specified section from loggcat."""
+        try:
+            cam_device = arc.adb_shell("logcat -d | grep 'Camera device'")
+        except error.CmdError:
+            cam_device = ''
+        try:
+            cam_disable = arc.adb_shell("logcat -d | grep 'disabled by policy'")
+        except error.CmdError:
+            cam_disable = ''
+
+        return [cam_device, cam_disable]
+
+    def did_cam_app_respond(self):
+        """
+        Check if the Camera app has responded to the start command via
+        data in the logs being populated.
+
+        @return: True/False, if the Camera has responded to the start command.
+
+        """
+        arc.adb_shell('pm grant com.google.android.GoogleCameraArc android.permission.ACCESS_COARSE_LOCATION')
+        arc.adb_shell('am start -a android.media.action.IMAGE_CAPTURE')
+        cam_logs = self._check_cam_status()
+        if cam_logs[0] or cam_logs[1]:
+            return True
+        return False
+
     def _test_Arc_cam_status(self, case):
         """
         Test if the Arc Camera has been opened, or not.
@@ -31,15 +78,6 @@ class policy_ArcVideoCaptureAllowed(
         @param case: bool, value of the VideoCaptureAllowed policy.
 
         """
-
-        #  The Camera can take a few seconds to respond, wait for it.
-        utils.poll_for_condition(
-            lambda: self.did_cam_app_respond(),
-            exception=error.TestFail('Camera APP did not respond.'),
-            timeout=10,
-            sleep_interval=1,
-            desc='Wait for Camera to respond.')
-
         #  Once the Camera is open, get the status from logcat.
         cam_device_resp, disabled_resp = self._check_cam_status()
 
@@ -50,32 +88,15 @@ class policy_ArcVideoCaptureAllowed(
         else:
             if ('opened successfully' in cam_device_resp or
                 'disabled by policy' not in disabled_resp):
-                raise error.TestFail(
-                    'Camera did launch when it should not have.')
-
-    def _launch_Arc_Cam(self):
-        """Grant the Camera location permission, and launch the Camera app."""
-        arc.adb_shell('pm grant com.google.android.GoogleCameraArc android.permission.ACCESS_COARSE_LOCATION')
-        arc.adb_shell('am start -a android.media.action.IMAGE_CAPTURE')
-
-    def _check_cam_status(self):
-        """Returns the specified section from loggcat."""
-        return [arc.adb_shell("logcat -d | grep 'Camera device'"),
-                arc.adb_shell("logcat -d | grep 'CAMERA_DISABLED'")]
-
-    def did_cam_app_respond(self):
-        """
-        Check if the Camera app has responded to the start command via
-        data in the logs being populated.
-
-        @return: True/False, if the Camera has responded to the start command.
-
-        """
-
-        cam_logs = self._check_cam_status()
-        if cam_logs[0] or cam_logs[1]:
-            return True
-        return False
+                # Sometimes due to timing the camera will still open, but then
+                # will quickly be closed. Check for this.
+                utils.poll_for_condition(
+                    lambda: self._cam_closed(),
+                    exception=error.TestFail(
+                        'Camera APP did not close or unstable.'),
+                    timeout=15,
+                    sleep_interval=3,
+                    desc='Wait for Camera to close.')
 
     def run_once(self, case):
         """
@@ -90,5 +111,9 @@ class policy_ArcVideoCaptureAllowed(
         self.setup_case(user_policies=pol,
                         arc_mode='enabled',
                         use_clouddpc_test=False)
+
+        # Allow the ARC container time to apply the policy...
+        time.sleep(15)
+
         self._launch_Arc_Cam()
         self._test_Arc_cam_status(case)
