e0d68236426ed17463e7f64a87b6f463547228b0
En-Shuo Hsu <enshuo@chromium.org>
2019-05-14 18:29:29 +0800

    [autotest] Add test for pinned stream recording
    - Check and active node is external mic
    - Play audio with sine wave f and f2 on Chameleon to mic and USB
    - Play audio with sine wave f on d
    - Record from and pinned to USB
    - Record from mic
    - Verify each of them still records the correct sine wave
    
    BUG=chromium:931117
    TEST=test_that -b ${BOARD} --fast --args=${CHAMELEON_IP} ${DUT_IP} \
    	audio_AudioPinnedStream.playback/recording \
         run on chromeos15-row14b-rack9-host12
    
    Change-Id: I937968f4872800c5198f665597b8f856eaa00e62
    Reviewed-on: https://chromium-review.googlesource.com/1614609
    Tested-by: En-Shuo Hsu <enshuo@google.com>
    Commit-Ready: En-Shuo Hsu <enshuo@google.com>
    Legacy-Commit-Queue: Commit Bot <commit-bot@chromium.org>
    Reviewed-by: Yu-Hsuan Hsu <yuhsuan@chromium.org>

--- server/site_tests/audio_AudioPinnedStream/audio_AudioPinnedStream.py
+++ server/site_tests/audio_AudioPinnedStream/audio_AudioPinnedStream.py
@@ -25,18 +25,16 @@ class audio_AudioPinnedStream(audio_test.AudioTest):
     """
     version = 1
     DELAY_BEFORE_RECORD_SECONDS = 0.5
-    RECORD_SECONDS = 3
+    PLAYBACK_RECORD_SECONDS = 3
+    RECORDING_RECORD_SECONDS = 6
     DELAY_AFTER_BINDING = 0.5
     DELAY_AFTER_SETTING = 5
 
-    def run_once(self, host):
+    def run_once(self, host, playback):
         """Running basic pinned stream audio tests.
 
         @param host: device under test host
         """
-        if not audio_test_utils.has_headphone(host):
-            return
-
         # [1330, 1330] sine wave
         golden_file = audio_test_data.SIMPLE_FREQUENCY_TEST_1330_FILE
         # [2000, 1000] sine wave
@@ -51,17 +49,26 @@ class audio_AudioPinnedStream(audio_test.AudioTest):
         widget_factory = chameleon_audio_helper.AudioWidgetFactory(
                 factory, host)
 
-        source = widget_factory.create_widget(
-            chameleon_audio_ids.CrosIds.HEADPHONE)
+        if playback:
+            source = widget_factory.create_widget(
+                    chameleon_audio_ids.CrosIds.HEADPHONE)
+            recorder = widget_factory.create_widget(
+                    chameleon_audio_ids.ChameleonIds.LINEIN)
+            usb_source = widget_factory.create_widget(
+                    chameleon_audio_ids.CrosIds.USBOUT)
+            usb_recorder = widget_factory.create_widget(
+                    chameleon_audio_ids.ChameleonIds.USBIN)
+        else:
+            source = widget_factory.create_widget(
+                    chameleon_audio_ids.ChameleonIds.LINEOUT)
+            recorder = widget_factory.create_widget(
+                    chameleon_audio_ids.CrosIds.EXTERNAL_MIC)
+            usb_source = widget_factory.create_widget(
+                    chameleon_audio_ids.ChameleonIds.USBOUT)
+            usb_recorder = widget_factory.create_widget(
+                    chameleon_audio_ids.CrosIds.USBIN)
 
-        recorder = widget_factory.create_widget(
-            chameleon_audio_ids.ChameleonIds.LINEIN)
         binder = widget_factory.create_binder(source, recorder)
-
-        usb_source = widget_factory.create_widget(
-            chameleon_audio_ids.CrosIds.USBOUT)
-        usb_recorder = widget_factory.create_widget(
-            chameleon_audio_ids.ChameleonIds.USBIN)
         usb_binder = widget_factory.create_binder(usb_source, usb_recorder)
 
         with chameleon_audio_helper.bind_widgets(usb_binder):
@@ -71,68 +78,69 @@ class audio_AudioPinnedStream(audio_test.AudioTest):
                 audio_facade = factory.create_audio_facade()
                 plugger = chameleon_board.get_audio_board().get_jack_plugger()
 
-                # Fix for chameleon rack hosts because audio jack is always plugged.
-                # crbug.com/955009
+                # Fix for chameleon rack hosts because audio jack is always
+                # plugged. (crbug.com/955009)
                 if plugger is None:
-                    audio_facade.set_selected_node_types(['HEADPHONE'], [''])
+                    if playback:
+                        audio_test_utils.check_and_set_chrome_active_node_types(
+                                audio_facade, 'HEADPHONE', None)
+                    else:
+                        audio_test_utils.check_and_set_chrome_active_node_types(
+                                audio_facade, None, 'MIC')
                     time.sleep(self.DELAY_AFTER_SETTING)
 
-                audio_test_utils.check_audio_nodes(
-                    audio_facade, (['HEADPHONE'], None))
-
-                logging.info('Setting playback data on Cros device')
+                logging.info('Setting playback data')
                 source.set_playback_data(golden_file)
                 usb_source.set_playback_data(usb_golden_file)
 
-                logging.info('Start playing %s on USB', usb_golden_file.path)
-                usb_source.start_playback(pinned=True)
-
-                logging.info('Start playing %s on headphone', golden_file.path)
+                logging.info('Start playing %s', golden_file.path)
                 source.start_playback()
 
+                logging.info('Start playing %s, pinned:%s',
+                        usb_golden_file.path, playback)
+                usb_source.start_playback(pinned=playback)
+
                 time.sleep(self.DELAY_BEFORE_RECORD_SECONDS)
-                logging.info('Start recording from Chameleon.')
 
-                # Not any two recorders on chameleon can record at the same
-                # time. USB and LineIn can but we would keep them separate
-                # here to keep things simple and change it when needed.
-                # Should still record 1330 sine wave from USB as it was set
-                # pinned on USB.
-                usb_recorder.start_recording()
-                time.sleep(self.RECORD_SECONDS)
-                usb_recorder.stop_recording()
-
-                # Should record [2000, 1000] sine from headphone.
+                logging.info('Start recording.')
                 recorder.start_recording()
-                time.sleep(self.RECORD_SECONDS)
-                recorder.stop_recording()
-
-                logging.info('Stopped recording from Chameleon.')
+                if playback:
+                    # Not any two recorders on chameleon can record at the same
+                    # time. USB and LineIn can but we would keep them separate
+                    # here to keep things simple and change it when needed.
+                    # Should still record [2000, 1000] sine wave from USB as
+                    # it was set pinned on USB.
+                    time.sleep(self.PLAYBACK_RECORD_SECONDS)
+                    recorder.stop_recording()
+                    usb_recorder.start_recording()
+                    time.sleep(self.PLAYBACK_RECORD_SECONDS)
+                    usb_recorder.stop_recording()
+                else:
+                    usb_recorder.start_recording(pinned=True)
+                    time.sleep(self.RECORDING_RECORD_SECONDS)
+                    recorder.stop_recording()
+                    usb_recorder.stop_recording(pinned=True)
 
                 audio_test_utils.dump_cros_audio_logs(
                         host, audio_facade, self.resultsdir, 'after_recording')
 
-                recorder.read_recorded_binary()
-                usb_recorder.read_recorded_binary()
-                logging.info('Read recorded binary from Chameleon.')
+        recorder.read_recorded_binary()
+        usb_recorder.read_recorded_binary()
 
         recorded_file = os.path.join(self.resultsdir, "recorded.raw")
         logging.info('Saving recorded data to %s', recorded_file)
-        recorder.save_file(recorded_file)
-
-        # Removes the beginning of recorded data. This is to avoid artifact
-        # caused by Chameleon codec initialization in the beginning of
-        # recording.
-        recorder.remove_head(0.5)
-
-        recorded_file = os.path.join(self.resultsdir, "recorded_clipped.raw")
-        logging.info('Saving clipped data to %s', recorded_file)
         recorder.save_file(recorded_file)
 
         usb_recorded_file = os.path.join(self.resultsdir, "usb_recorded.raw")
         logging.info('Saving recorded data to %s', usb_recorded_file)
         usb_recorder.save_file(usb_recorded_file)
 
-        audio_test_utils.check_recorded_frequency(golden_file, recorder)
-
+        if playback:
+            audio_test_utils.check_recorded_frequency(golden_file, recorder)
+        else:
+            # The signal quality from chameleon will get affected when usb is
+            # also connected. We'll just lower the quality bar here, as it is
+            # not the main point of this test.
+            audio_test_utils.check_recorded_frequency(
+                    golden_file, recorder, second_peak_ratio=0.3)
         audio_test_utils.check_recorded_frequency(usb_golden_file, usb_recorder)
