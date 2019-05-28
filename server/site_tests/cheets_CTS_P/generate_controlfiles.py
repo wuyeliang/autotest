694036923625d3b6a484164be475bf89385918bf
ChromeOS Developer <yoshiki@google.com>
2019-05-28 15:07:57 +0900

    Enable default apps on CtsAppSecurityHostTestCases
    CtsAppSecurityHostTestCases needs Files.app to run itself
    and Files.app is included in the default apps. This CL enables
    the default apps on Chrome when the tests run.
    
    BUG=b:132926689
    TEST=Ran CtsAppSecurityHostTestCases and confirms the default apps
        running.
    Change-Id: Ic17a64a11703b29fd1022cb600dc061c4f9067be
    Reviewed-on: https://chromium-review.googlesource.com/1634010
    Commit-Ready: ChromeOS CL Exonerator Bot <chromiumos-cl-exonerator@appspot.gserviceaccount.com>
    Tested-by: Kazuhiro Inaba <kinaba@chromium.org>
    Legacy-Commit-Queue: Commit Bot <commit-bot@chromium.org>
    Reviewed-by: Satoshi Niwa <niwa@chromium.org>
    Reviewed-by: Kazuhiro Inaba <kinaba@chromium.org>
    (cherry picked from commit 0fa901f2fe4de0010365161457946ae9367583fe)
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/autotest/+/1646797
    Commit-Queue: Kazuhiro Inaba <kinaba@chromium.org>

--- server/site_tests/cheets_CTS_P/generate_controlfiles.py
+++ server/site_tests/cheets_CTS_P/generate_controlfiles.py
@@ -63,6 +63,9 @@ _CONTROLFILE_TEMPLATE = Template(
     {%- if max_retries != None %}
             max_retry={{max_retries}},
     {%- endif %}
+    {%- if enable_default_apps %}
+            enable_default_apps=True,
+    {%- endif %}
             needs_push_media={{needs_push_media}},
             tag='{{tag}}',
             test_name='{{name}}',
@@ -226,6 +229,11 @@ _MEDIA_MODULES = [
     'CtsMediaBitstreamsTestCases',
 ]
 _NEEDS_PUSH_MEDIA = _MEDIA_MODULES + [_ALL]
+
+# Modules that are known to need the default apps of Chrome (eg. Files.app).
+_ENABLE_DEFAULT_APPS = [
+    'CtsAppSecurityHostTestCases',
+]
 
 # Run `eject` for (and only for) each device with RM=1 in lsblk output.
 _EJECT_REMOVABLE_DISK_COMMAND = (
@@ -1021,6 +1029,13 @@ def needs_push_media(modules):
     return False
 
 
+def enable_default_apps(modules):
+    """Oracle to determine if to enable default apps (eg. Files.app)."""
+    if modules.intersection(set(_ENABLE_DEFAULT_APPS)):
+        return True
+    return False
+
+
 def get_controlfile_content(combined,
                             modules,
                             abi,
@@ -1069,6 +1084,7 @@ def get_controlfile_content(combined,
         build=build,
         abi=abi,
         needs_push_media=needs_push_media(modules),
+        enable_default_apps=enable_default_apps(modules),
         tag=tag,
         uri=uri,
         DOC=get_doc(modules, abi, is_public),
