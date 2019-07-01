f36879ae0df730e7fbe2a1e89784f6e7e9ce7618
David Haddock <dhaddock@chromium.org>
2019-07-01 16:41:56 -0700

    Rewrite Files app autotest.
    The test was failing in various places because of randomly failing to
    find the autotest_ext. I've added a new function that all javascript calls
    will go through that will check for the random keyerror that appears.
    Previously I wrapped each javascript call in its own try/except.
    
    I have also main the main function easier to read by putting the
    _wait_for_element calls into other functions.
    
    BUG=None
    TEST=desktopui_FilesApp
    
    Change-Id: I930615a73f1aeddd3dd89c82d7409bb89caa3376
    Reviewed-on: https://chromium-review.googlesource.com/1684450
    Tested-by: David Haddock <dhaddock@chromium.org>
    Commit-Ready: David Haddock <dhaddock@chromium.org>
    Legacy-Commit-Queue: Commit Bot <commit-bot@chromium.org>
    Reviewed-by: Katherine Threlkeld <kathrelkeld@chromium.org>

--- client/site_tests/desktopui_FilesApp/desktopui_FilesApp.py
+++ client/site_tests/desktopui_FilesApp/desktopui_FilesApp.py
@@ -4,6 +4,7 @@
 
 import os
 import logging
+import time
 
 from autotest_lib.client.bin import test, utils
 from autotest_lib.client.common_lib import error
@@ -29,123 +30,6 @@ class desktopui_FilesApp(test.test):
     _NEW_FOLDER = 'New folder'
 
 
-    def _wait_for_element_to_be_visible(self, cr, role, name, timeout=10,
-                                        err_str=None):
-        """
-        Wait for an element to appear on screen.
-
-        @param cr: The chrome browser running
-        @param role: The chrome.automation role we want to look for
-        @param name: The chrome.automation name we want to look for
-        @param timeout: How long to wait
-        @param err_str: The error string to pass to the TimeoutException
-
-        """
-        def finished_loading():
-            """Check if an element has finished loading."""
-            try:
-                visible = cr.autotest_ext.EvaluateJavaScript("""
-                   root.find({attributes: {role: '%s', name: '%s'}});
-                   """ % (role, name))
-                logging.info(visible)
-                return visible is not None
-            except KeyError as e:
-                logging.exception('Could not find autotest_ext')
-                return False
-        utils.poll_for_condition(condition=finished_loading, timeout=timeout,
-                                 desc=err_str)
-
-
-    def _launch_app(self, cr, app_id):
-        """
-        Launch the app/extension by its ID and check that it opens.
-
-        @param cr: The chrome browser instance that is running.
-        @param app_id: The app ID of the app to launch.
-
-        """
-        ext = cr.autotest_ext
-        ext.ExecuteJavaScript("""
-            chrome.autotestPrivate.launchApp('%s', function(){})""" % app_id)
-
-        def check_open():
-            """Check if the app is running."""
-            try:
-                cr.browser.extensions.GetByExtensionId(app_id)
-            except KeyError:
-                return False
-            return True
-
-        try:
-            utils.poll_for_condition(condition=check_open, timeout=60)
-        except utils.TimeoutError:
-            raise error.TestFail('Could not open app: %s' % app_id)
-
-
-    def _load_automation_root(self, cr):
-        """
-        Load the chrome.automation API and get the on screen elements.
-
-        @param cr: The browser instance that is running.
-
-        """
-        cr.autotest_ext.ExecuteJavaScript("""
-            var root;
-            chrome.automation.getDesktop(r => root = r);
-            """)
-
-
-    def _open_downloads(self, cr):
-        """
-        Open the Downloads folder in the Files app.
-
-        @param cr: The browser instance that is running.
-
-        """
-        self._wait_for_element_to_be_visible(cr, self._STATIC_TEXT,
-                                             self._DOWNLOADS,
-                                             err_str='Downloads to appear')
-        cr.autotest_ext.EvaluateJavaScript("""
-            downloads = root.find({attributes: {role: '%s',
-                                                name: '%s'}});
-            downloads.doDefault();
-            """ % (self._STATIC_TEXT, self._DOWNLOADS))
-
-
-    def _find_all_by_role(self, cr, role):
-        """
-        Finds all the visible elements of a certain role type.
-
-        @param cr: The browser instance that is running
-        @param role: The type of role to search for.
-
-        """
-        try:
-            roles = cr.autotest_ext.EvaluateJavaScript("""
-                var root;
-                chrome.automation.getDesktop(r => root = r);
-                root.findAll({attributes: {role: '%s'}}).map(node => node.name);
-                """ % role)
-            logging.info(roles)
-            return roles
-        except Exception:
-            logging.exception('Failed to get elements by role.')
-
-
-    def _open_more_options(self, cr):
-        """
-        Opens the three dot menu of the Files app.
-
-        @param cr: The browser instance that is open.
-
-        """
-        cr.autotest_ext.EvaluateJavaScript("""
-            more = root.find({attributes: {role: '%s',
-                                           name: 'More\u2026'}});
-            more.doDefault();
-            """ % self._BUTTON)
-
-
     def cleanup(self):
         """Remove temporary files created during test."""
         text_file = os.path.join(self._DOWNLOADS_PATH, self._TEXT_FILE)
@@ -156,29 +40,150 @@ class desktopui_FilesApp(test.test):
             logging.info('Failed to delete files in cleanup. Ignoring.')
 
 
+    def evaluate_javascript(self, code, retries=3):
+        """Executes javascript and returns some result.
+
+        Occasionally calls to EvaluateJavascript on the autotest_ext will fail
+        to find the extension. Instead of wrapping every call in a try/except,
+        calls will go through this function instead.
+
+        @param code: The javascript string to execute
+        @param retries: The number of times to retry the code
+
+        """
+        for i in range(0, retries):
+            try:
+                result = self.cr.autotest_ext.EvaluateJavaScript(code)
+                return result
+            except KeyError as e:
+                logging.exception('Could not find autotest_ext')
+
+            time.sleep(1)
+
+        raise error.TestFail('Could not execute "%s" in %d retires' %
+                             (code, retries))
+
+
+    def _wait_for_element_to_be_visible(self, role, name, timeout=10,
+                                        err_str='element to load'):
+        """
+        Wait for an element to appear on screen.
+
+        @param role: The chrome.automation role we want to look for
+        @param name: The chrome.automation name we want to look for
+        @param timeout: How long to wait
+        @param err_str: The error string to pass to the TimeoutException
+
+        """
+        find_js = "root.find({attributes: {role: '%s', name: '%s'}});" %\
+                  (role, name)
+
+        def finished_loading():
+            """Check if an element has finished loading."""
+            visible = self.evaluate_javascript(find_js)
+            return visible is not None
+        utils.poll_for_condition(condition=finished_loading, timeout=timeout,
+                                 desc=err_str)
+
+
+    def _click_element(self, role, name):
+        """Click an element by role/name combination.
+
+        @param role: The chrome.automation role we want to look for
+        @param name: The chrome.automation name we want to look for
+
+        """
+        click_js = """
+            element = root.find({attributes: {role: '%s',
+                                              name: '%s'}});
+            element.doDefault(); """ % (role, name)
+        self.evaluate_javascript(click_js)
+
+
+    def _launch_app(self, id):
+        """
+        Launch the app/extension by its ID and check that it opens.
+
+        @param id: The app ID of the app to launch.
+
+        """
+        launch_js = "chrome.autotestPrivate.launchApp('%s', function(){})" % id
+        self.evaluate_javascript(launch_js)
+
+        def check_open():
+            """Check if the app is running."""
+            try:
+                self.cr.browser.extensions.GetByExtensionId(id)
+            except KeyError:
+                return False
+            return True
+
+        try:
+            utils.poll_for_condition(condition=check_open, timeout=60)
+        except utils.TimeoutError:
+            raise error.TestFail('Could not open app: %s' % id)
+
+
+    def _load_automation_root(self):
+        """Load the chrome.automation API and get the on screen elements."""
+        load_cmd = "var root; chrome.automation.getDesktop(r => root = r);"
+        self.evaluate_javascript(load_cmd)
+
+
+    def _find_all_by_role(self, role):
+        """
+        Finds all the visible elements of a certain role type.
+
+        @param role: The type of role to search for.
+
+        """
+        find_all_js = """
+            var root;
+            chrome.automation.getDesktop(r => root = r);
+            root.findAll({attributes: {role: '%s'}}).map(node => node.name);
+            """ % role
+        roles = self.evaluate_javascript(find_all_js)
+        logging.info(roles)
+        return roles
+
+
+    def _open_downloads(self):
+        """Open the Downloads folder in the Files app."""
+        self._wait_for_element_to_be_visible(self._BUTTON,
+                                             self._MY_FILES,
+                                             err_str='Files to load')
+        self._wait_for_element_to_be_visible(self._STATIC_TEXT,
+                                             self._DOWNLOADS,
+                                             err_str='Downloads to appear')
+        self._click_element(self._STATIC_TEXT, self._DOWNLOADS)
+        self._wait_for_element_to_be_visible(self._STATIC_TEXT,
+                                             self._TEXT_FILE,
+                                             err_str='.txt to show')
+
+
+    def _open_more_options(self):
+        """Opens the three dot menu of the Files app."""
+        self._click_element(self._BUTTON, 'More\u2026')
+        self._wait_for_element_to_be_visible(self._STATIC_TEXT,
+                                             self._NEW_FOLDER,
+                                             err_str='New folder item')
+
+
     def run_once(self):
         """Main entry point of test."""
         with chrome.Chrome(autotest_ext=True,
                            disable_default_apps=False) as cr:
             # Setup
+            self.cr = cr
             utils.write_one_line(os.path.join(self._DOWNLOADS_PATH,
                                               self._TEXT_FILE), 'blahblah')
-            self._load_automation_root(cr)
+            self._load_automation_root()
 
             try:
                 # Files app testing
-                self._launch_app(cr, self._FILES_APP_ID)
-                self._wait_for_element_to_be_visible(cr, self._BUTTON,
-                                                     self._MY_FILES,
-                                                     err_str='Files to load')
-                self._open_downloads(cr)
-                self._wait_for_element_to_be_visible(cr, self._STATIC_TEXT,
-                                                     self._TEXT_FILE,
-                                                     err_str='.txt to show')
-                self._open_more_options(cr)
-                self._wait_for_element_to_be_visible(cr, self._STATIC_TEXT,
-                                                     self._NEW_FOLDER,
-                                                     err_str='New folder item')
+                self._launch_app(self._FILES_APP_ID)
+                self._open_downloads()
+                self._open_more_options()
             except (ValueError, utils.TimeoutError):
                 logging.exception('Failed to verify Files app is available.')
                 try:
@@ -188,5 +193,5 @@ class desktopui_FilesApp(test.test):
                 raise
             finally:
                 logging.info('Extra logging for debugging')
-                self._find_all_by_role(cr, self._BUTTON)
-                self._find_all_by_role(cr, self._STATIC_TEXT)
+                self._find_all_by_role(self._BUTTON)
+                self._find_all_by_role(self._STATIC_TEXT)
