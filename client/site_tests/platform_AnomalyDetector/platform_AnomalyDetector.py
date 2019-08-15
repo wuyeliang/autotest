8aebd7f3ae1af90b5d01666006a76e4f3b212b93
Miriam Zimmerman <mutexlox@google.com>
2019-08-14 17:22:43 -0700

    autotest-tests: Deflake platform_AnomalyDetector
    By only sleeping for 0.1s, platform_AnomalyDetector is extremely
    vulnerable to flakiness. Allow more time to pass before failing to
    improve reliability.
    
    BUG=chromium:983725
    TEST=test_that -b eve $DEVICE_IP platform_AnomalyDetector
    
    Change-Id: Ia00226c84d8df4fee31e010c662d06938bf90161
    Reviewed-on: https://chromium-review.googlesource.com/1754919
    Tested-by: Miriam Zimmerman <mutexlox@chromium.org>
    Commit-Ready: ChromeOS CL Exonerator Bot <chromiumos-cl-exonerator@appspot.gserviceaccount.com>
    Legacy-Commit-Queue: Commit Bot <commit-bot@chromium.org>
    Reviewed-by: Brian Norris <briannorris@chromium.org>
    Reviewed-by: J Kardatzke <jkardatzke@chromium.org>
    Reviewed-by: Jeffrey Kardatzke <jkardatzke@google.com>

--- client/site_tests/platform_AnomalyDetector/platform_AnomalyDetector.py
+++ client/site_tests/platform_AnomalyDetector/platform_AnomalyDetector.py
@@ -3,7 +3,9 @@
 # found in the LICENSE file.
 
 import os
+import time
 from autotest_lib.client.bin import test, utils
+from autotest_lib.client.cros import upstart
 
 class platform_AnomalyDetector(test.test):
     "Tests the anomaly detector daemon"
@@ -12,16 +14,23 @@ class platform_AnomalyDetector(test.test):
     def run_once(self):
         "Runs the test once"
 
-        # Restart the anomaly detector daemon, trigger a test kernel warning,
-        # and verify that a warning file is created.
-        utils.system("stop anomaly-detector")
+        # Restart the anomaly detector daemon (to clear its cache of
+        # "already-seen warnings" and ensure this one is logged)
+        upstart.restart_job("anomaly-detector")
+
+        # Give enough time for the anomaly detector to open the journal and scan
+        # to the end. (otherwise, it will miss the warning message).
+        # TODO(https://crbug.com/983725): Check that it's opened the journal.
+        time.sleep(0.5)
+
+        # Delete old kernel warning files to distinguish this one.
         utils.system("rm -rf /var/spool/crash/kernel_warning*")
-        utils.system("start anomaly-detector")
-        utils.system("sleep 0.1")
+
         lkdtm = "/sys/kernel/debug/provoke-crash/DIRECT"
         if os.path.exists(lkdtm):
             utils.system("echo WARNING > %s" % (lkdtm))
         else:
             utils.system("echo warning > /proc/breakme")
-        utils.system("sleep 0.1")
-        utils.system("test -f /var/spool/crash/kernel_warning*.kcrash")
+
+        cmd = "test -f /var/spool/crash/kernel_warning*.kcrash"
+        utils.poll_for_condition_ex(lambda: utils.system(cmd) == 0)
