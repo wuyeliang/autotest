e3340762970033ff4140e2dfa4549956def89cd6
Jae Hoon Kim <kimjae@chromium.org>
2019-08-19 15:21:49 -0700

    Milestones to EOL added into update_engine_client --eol_status printout
    Initiative to include the milestones to EOL is being made within
    update_engine. The update_engine_client prints out the
    `MILESTONES_TO_EOL` after `EOL_STATUS`. The changes for update_engine
    can be followed at the CQ dependency.
    
    Nano Omaha devserver feeds back EOL response with milestone to EOL.
    
    Addition control test is added to test for milestones to EOL when the
    EOL is not at it's end-of-life yet. This is a new feature test.
    
    BUG=chromium:994999
    TEST=test_that -b $BOARD $IP autoupdate_EOL
    TEST=test_that -b $BOARD $IP autoupdate_EOL.milestones
    
    Cq-Depend:chromium:1759285
    Change-Id: Ic3028e19047bcd4d96802c6d77da7b8985b183dc
    Reviewed-on: https://chromium-review.googlesource.com/1761371
    Tested-by: Jae Hoon Kim <kimjae@chromium.org>
    Commit-Ready: ChromeOS CL Exonerator Bot <chromiumos-cl-exonerator@appspot.gserviceaccount.com>
    Legacy-Commit-Queue: Commit Bot <commit-bot@chromium.org>
    Reviewed-by: Jae Hoon Kim <kimjae@chromium.org>
    Reviewed-by: David Haddock <dhaddock@chromium.org>

--- client/cros/update_engine/nano_omaha_devserver.py
+++ client/cros/update_engine/nano_omaha_devserver.py
@@ -23,12 +23,15 @@ def _split_url(url):
 class NanoOmahaDevserver(object):
     """A simple Omaha instance that can be setup on a DUT in client tests."""
 
-    def __init__(self, eol=False, failures_per_url=1, backoff=False,
-                 num_urls=2):
+    def __init__(self, eol=None, milestones_to_eol=None, failures_per_url=1,
+                 backoff=False, num_urls=2):
         """
         Create a nano omaha devserver.
 
-        @param eol: True if we should return a response with _eol flag.
+        @param eol: Not None if we should return a response with _eol flag.
+        @param milestones_to_eol: This param is second to eol param, meaning if
+                                  eol param isn't set this param has no effect.
+                                  Default is 0 if None and eol param supplied.
         @param failures_per_url: how many times each url can fail.
         @param backoff: Whether we should wait a while before trying to
                         update again after a failure.
@@ -36,6 +39,7 @@ class NanoOmahaDevserver(object):
 
         """
         self._eol = eol
+        self._milestones_to_eol = milestones_to_eol
         self._failures_per_url = failures_per_url
         self._backoff = backoff
         self._num_urls = num_urls
@@ -55,7 +59,8 @@ class NanoOmahaDevserver(object):
             <daystart elapsed_seconds="44801"/>
             <app appid="$appid" status="ok">
               <ping status="ok"/>
-              <updatecheck _eol="eol" status="noupdate"/>
+              <updatecheck _eol="$eol" _milestones_to_eol="$milestones_to_eol"
+               status="noupdate"/>
             </app>
           </response>
         """)
@@ -108,9 +113,14 @@ class NanoOmahaDevserver(object):
                 _rollback="$is_rollback"
                 """)
 
-        # IF EOL, return a simplified response with _eol tag.
-        if self._eol:
-            return EOL_TEMPLATE.substitute(appid=appid)
+        # IF EOL or milestones to EOL, return a simplified response with _eol
+        # tag and _milestones_to_eol tag.
+        if self._eol is not None:
+            self._milestones_to_eol = self._milestones_to_eol or 0
+            return EOL_TEMPLATE.substitute(
+                       appid=appid,
+                       eol=self._eol,
+                       milestones_to_eol=self._milestones_to_eol)
 
         template_keys = {}
         template_keys['is_delta'] = str(self._is_delta).lower()
