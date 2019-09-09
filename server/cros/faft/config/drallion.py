d1b70f679222de526770b280076fd569632383d9
Chiranjeevi Rapolu <chiranjeevi.rapolu@intel.com>
2019-09-09 09:24:02 -0700

    drallion: Add FAFT config for drallion board
    Add FAFT configs for the drallion board that do not have
    a Chromium EC.
    
    As an initial version this is a cloned version of sarien.
    
    BUG=b:138478838
    TEST=pass firmware_FAFTSetup on drallion
    
    Change-Id: I7f879011edac6112f2605afa31b347c4cebe7c95
    Signed-off-by: Chiranjeevi Rapolu <chiranjeevi.rapolu@intel.com>
    Reviewed-on: https://chromium-review.googlesource.com/1793060
    Tested-by: Mathew King <mathewk@chromium.org>
    Commit-Ready: ChromeOS CL Exonerator Bot <chromiumos-cl-exonerator@appspot.gserviceaccount.com>
    Legacy-Commit-Queue: Commit Bot <commit-bot@chromium.org>
    Reviewed-by: Mathew King <mathewk@chromium.org>
    Reviewed-by: Abe Levkoy <alevkoy@chromium.org>

---
+++ server/cros/faft/config/drallion.py
@@ -0,0 +1,31 @@
+# Copyright 2019 Google LLC
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""FAFT configuration overrides for Drallion."""
+
+class Values(object):
+    """FAFT config values for Drallion."""
+    firmware_screen = 15
+    delay_reboot_to_ping = 40
+    hold_pwr_button_poweron = 1.2
+    has_lid = True
+    lid_wake_from_power_off = False
+    spi_voltage = 'pp3300'
+    # Not a Chrome EC, do not expect keyboard via EC
+    chrome_ec = False
+    ec_capability = []
+    has_keyboard = False
+    power_button_dev_switch = True
+    smm_store = False
+    # The EC image is stored in the AP SPI chip, so flashrom -p ec won't work.
+    ap_access_ec_flash = False
+    # Depthcharge USB stack can drop keys that come in too fast and get stuck
+    # exiting developer mode if the delay for confirmation screen is too short.
+    confirm_screen = 11
+    # There is no key sequence to force memory retraining.
+    rec_force_mrc = False
+    ap_up_after_cr50_reboot = False
+    ec_forwards_short_pp_press = True
+    serial_number_length = 7
+    has_diagnostics_image = True
