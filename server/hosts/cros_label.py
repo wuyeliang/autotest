6833cafa48869f9389ec1aa5ae080e33b113f660
Xixuan Wu <xixuan@google.com>
2019-09-17 10:37:17 -0700

    autotest: fix chameleon logging.
    It raises error like "TypeError: not enough arguments for format
    string".
    
    BUG=chromium:1004500
    TEST=None
    
    Change-Id: Ic727977afc35c667e3bb8066d0ff740b0788968c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/autotest/+/1809492
    Tested-by: Xixuan Wu <xixuan@chromium.org>
    Legacy-Commit-Queue: Commit Bot <commit-bot@chromium.org>
    Reviewed-by: Garry Wang <xianuowang@chromium.org>
    Commit-Queue: Xixuan Wu <xixuan@chromium.org>

--- server/hosts/cros_label.py
+++ server/hosts/cros_label.py
@@ -251,9 +251,12 @@ class ChameleonLabel(base_label.BaseLabel):
         # TODO(crbug.com/995900) -- debug why chameleon label is flipping
         try:
             logging.info("has_chameleon %s", has_chameleon)
-            logging.info("chameleon_host_list %s", getattr(host, "_chameleon_host_list", "NO_ATTRIBUTE"))
-            logging.info("host: %s chameleon_list %s", getattr(host, "chameleon_list", "NO_ATTRIBUTE"))
-            logging.info("host: %s multi_chameleon", getattr(host, "multi_chameleon", "NO_ATTRIBUTE"))
+            logging.info("chameleon_host_list %s",
+                         getattr(host, "_chameleon_host_list", "NO_ATTRIBUTE"))
+            logging.info("chameleon_list %s",
+                         getattr(host, "chameleon_list", "NO_ATTRIBUTE"))
+            logging.info("multi_chameleon %s",
+                         getattr(host, "multi_chameleon", "NO_ATTRIBUTE"))
         except:
             pass
         return has_chameleon
