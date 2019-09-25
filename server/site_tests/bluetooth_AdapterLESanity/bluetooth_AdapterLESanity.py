3395c1e4c00e0e97f8ecc778726da4d549aa0c9b
Shijin Abraham <shijinabraham@google.com>
2019-09-24 17:14:44 -0700

    bluetooth: fix package path
    Using the full path of package since a failure was seen
    in the lab.
    
    BUG=chromium:992796
    TEST=Tested locally
    
    Change-Id: I85746ef17c56fe94bdc17b75497b10cdb370e696
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/autotest/+/1824071
    Tested-by: Shijin Abraham <shijinabraham@google.com>
    Auto-Submit: Shijin Abraham <shijinabraham@google.com>
    Reviewed-by: Yun-Hao Chung <howardchung@google.com>
    Legacy-Commit-Queue: Commit Bot <commit-bot@chromium.org>
    Commit-Queue: ChromeOS CL Exonerator Bot <chromiumos-cl-exonerator@appspot.gserviceaccount.com>

--- server/site_tests/bluetooth_AdapterLESanity/bluetooth_AdapterLESanity.py
+++ server/site_tests/bluetooth_AdapterLESanity/bluetooth_AdapterLESanity.py
@@ -10,8 +10,8 @@ import time
 from autotest_lib.server.cros.bluetooth.bluetooth_adapter_quick_tests import \
      BluetoothAdapterQuickTests
 
-from server.site_tests.bluetooth_AdapterPairing.bluetooth_AdapterPairing  import bluetooth_AdapterPairing
-from server.site_tests.bluetooth_AdapterHIDReports.bluetooth_AdapterHIDReports  import bluetooth_AdapterHIDReports
+from autotest_lib.server.site_tests.bluetooth_AdapterPairing.bluetooth_AdapterPairing  import bluetooth_AdapterPairing
+from autotest_lib.server.site_tests.bluetooth_AdapterHIDReports.bluetooth_AdapterHIDReports  import bluetooth_AdapterHIDReports
 
 class bluetooth_AdapterLESanity(BluetoothAdapterQuickTests,
         bluetooth_AdapterPairing,
