794607a802a8751cf4f1c4696d67eb3a73f0a5e3
Max Timkovich <timkovich@chromium.org>
2019-09-25 17:14:36 -0700

    Mute DUTs during camerabox initialization
    Some of the camerabox CTS tests produce audio and were being a nuisance to
    the lab team. Not all lab devices have audio jacks to put dongles in
    (e.g. Nocturne) which was our previous method of disabling audio.
    
    TEST=this
    
    Change-Id: I8c4226e527cf5e34f4f91d1ce4d998b7d15191b1
    Reviewed-on: https://chromium-review.googlesource.com/1826046
    Tested-by: Max Timkovich <timkovich@chromium.org>
    Commit-Ready: ChromeOS CL Exonerator Bot <chromiumos-cl-exonerator@appspot.gserviceaccount.com>
    Legacy-Commit-Queue: Commit Bot <commit-bot@chromium.org>
    Reviewed-by: Kazuhiro Inaba <kinaba@chromium.org>

--- server/site_tests/cheets_CTS_P/cheets_CTS_P.py
+++ server/site_tests/cheets_CTS_P/cheets_CTS_P.py
@@ -132,6 +132,9 @@ class cheets_CTS_P(tradefed_test.TradefedTest):
         for dut in self.dut_fixtures:
             dut.initialize()
 
+        for host in self._hosts:
+            host.run('cras_test_client --mute 1')
+
     def initialize(self,
                    camera_facing=None,
                    bundle=None,
