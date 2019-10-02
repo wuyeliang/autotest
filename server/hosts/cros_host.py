3eeeab3ddec99131f2fd6fc7bf5a1a7b3fbe13a0
Ruben Rodriguez Buchillon <coconutruben@chromium.org>
2019-10-02 15:29:58 -0700

    make determination of RPM type dependent on pd support control
    This change replaces the previous logic using the info-store servo type
    with querying the servo instance (if it is initialized) whether it
    supports pd control.
    
    BUG=chromium:1009616
    TEST=None (previous CL tests pd query to be correct)
    
    Change-Id: Ifc42fdf6d6ffedf44adb6bdc356f36874ec63887
    Signed-off-by: Ruben Rodriguez Buchillon <coconutruben@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/autotest/+/1836572
    Reviewed-by: Garry Wang <xianuowang@chromium.org>

--- server/hosts/cros_host.py
+++ server/hosts/cros_host.py
@@ -144,10 +144,6 @@ class CrosHost(abstract_ssh.AbstractSSHHost):
                                 POWER_CONTROL_SERVO,
                                 POWER_CONTROL_MANUAL)
 
-    # CCD_SERVO: The string of servo type to compare with the return value of
-    #            self.servo.get_servo_version() to decide default power method.
-    CCD_SERVO = 'servo_v4_with_ccd_cr50'
-
     _RPM_OUTLET_CHANGED = 'outlet_changed'
 
     # URL pattern to download firmware image.
@@ -2032,13 +2028,10 @@ class CrosHost(abstract_ssh.AbstractSSHHost):
         """
         if not self._default_power_method:
             self._default_power_method = self.POWER_CONTROL_RPM
-            info = self.host_info_store.get()
-            if info.attributes.get('servo_type') == self.CCD_SERVO:
-                if self.servo:
-                    self._default_power_method = self.POWER_CONTROL_CCD
-                else:
-                    logging.warn('CCD servo detected but failed to apply CCD'
-                                 ' servo RPM method due to servo instance'
-                                 ' was not initialized, fall back to normal'
-                                 ' RPM method.')
+            if self.servo and self.servo.supports_built_in_pd_control():
+                self._default_power_method = self.POWER_CONTROL_CCD
+            else:
+                logging.debug('Either servo is unitialized or the servo '
+                              'setup does not support pd controls. Falling '
+                              'back to default RPM method.')
         return self._default_power_method
