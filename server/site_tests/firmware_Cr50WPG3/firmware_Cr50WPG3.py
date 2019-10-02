3b2edf9a0bb135dea07b23533a0c7ce38fb4013e
Mary Ruthven <mruthven@chromium.org>
2019-10-02 14:24:14 -0700

    faft-cr50: add wp g3 test
    On some boards Cr50 can't deassert WP in G3, because the external WP
    pullup isn't powered when the EC is off. Like the Rdd in G3 issue we
    can't fix these hardware issues on released boards. We need to fix it on
    new boards and track it on existing boards. This change adds a test to
    verify Cr50 can disable WP in G3.
    
    BUG=b:122538302
    BRANCH=none
    TEST=run on octopus and atlas
    
    Change-Id: I1c2fe017c2cefe66a511d1d0c386447d4462f98b
    Signed-off-by: Mary Ruthven <mruthven@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/autotest/+/1835028
    Reviewed-by: Dana Goyette <dgoyette@chromium.org>

---
+++ server/site_tests/firmware_Cr50WPG3/firmware_Cr50WPG3.py
@@ -0,0 +1,53 @@
+# Copyright 2019 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import logging
+import time
+
+from autotest_lib.client.common_lib import error
+from autotest_lib.server.cros.faft.cr50_test import Cr50Test
+
+
+class firmware_Cr50WPG3(Cr50Test):
+    """Verify WP in G3."""
+    version = 1
+
+    WAIT_FOR_STATE = 10
+
+    def run_once(self):
+        """Verify WP in G3."""
+        if self.check_cr50_capability(['wp_on_in_g3'], suppress_warning=True):
+            raise error.TestNAError('WP not pulled up in G3')
+        if 'servo_micro' not in self.servo.get_servo_version(True):
+            raise error.TestNAError('Need servo flex to check WP')
+
+        self.fast_open(True)
+
+        # Stop forcing wp using servo, so we can set it with ccd.
+        self.servo.set_nocheck('fw_wp_state', 'reset')
+        self.servo.set_nocheck('fw_wp_vref', 'off')
+
+        # disable write protect
+        self.cr50.send_command('wp disable atboot')
+
+        # Verify we can see it's disabled. This should always work. If it
+        # doesn't, it may be a setup issue.
+        servo_wp_s0 = self.servo.get('fw_wp_state')
+        logging.info('servo wp: %s', servo_wp_s0)
+        if servo_wp_s0 != 'off':
+            raise error.TestError("WP isn't disabled in S0")
+
+        self.faft_client.System.RunShellCommand('poweroff')
+        time.sleep(self.WAIT_FOR_STATE)
+        if hasattr(self, 'ec'):
+            self.ec.send_command('hibernate')
+            time.sleep(self.WAIT_FOR_STATE)
+
+        servo_wp_g3 = self.servo.get('fw_wp_state')
+        logging.info('servo wp: %s', servo_wp_g3)
+        self._try_to_bring_dut_up()
+        # Some boards don't power the EC_WP signal in G3, so it can't be
+        # disabled by cr50 in G3.
+        if servo_wp_g3 != 'off':
+            raise error.TestFail("WP can't be disabled by Cr50 in G3")
