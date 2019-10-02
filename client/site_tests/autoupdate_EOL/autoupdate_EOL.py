91e5922dcf7068ff9d443054b57441eb2fd179d5
Regan Hsu <hsuregan@google.com>
2019-10-01 17:18:23 -0700

    autotest: Add integration test for EOL notifications.
    BUG=chromium:995889
    TEST=test_that -b $B $IP autoupdate_EOL
    TEST=test_that -b $B $IP autoupdate_EOL.approaching_eol
    TEST=test_that -b $B $IP autoupdate_EOL.future_eol
    
    Change-Id: If29a7ed8f75e9034b44b9b3983c398b665ce5029
    Signed-off-by: Regan Hsu <hsuregan@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/1834921
    Legacy-Commit-Queue: Commit Bot <commit-bot@chromium.org>
    Reviewed-by: David Haddock <dhaddock@chromium.org>
    (cherry picked from commit 62ce55e036bef358b8f4422ec3e67960bbff73f0)
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/autotest/+/1958981
    Commit-Queue: David Haddock <dhaddock@chromium.org>
    Tested-by: David Haddock <dhaddock@chromium.org>

--- client/site_tests/autoupdate_EOL/autoupdate_EOL.py
+++ client/site_tests/autoupdate_EOL/autoupdate_EOL.py
@@ -1,9 +1,11 @@
 # Copyright 2018 The Chromium OS Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
+import datetime
 
 from autotest_lib.client.bin import utils
 from autotest_lib.client.common_lib import error
+from autotest_lib.client.common_lib.cros import chrome
 from autotest_lib.client.cros.update_engine import nano_omaha_devserver
 from autotest_lib.client.cros.update_engine import update_engine_test
 
@@ -11,8 +13,12 @@ class autoupdate_EOL(update_engine_test.UpdateEngineTest):
     """Tests end of life (EOL) behaviour."""
     version = 1
 
+    _EXPECTED_FINAL_TITLE = 'Final software update'
+    _DAYS_BEFORE_EOL_START_WARNING = 180
+    # Value within {} expected to be number of days since epoch.
     _EXPECTED_EOL_DATE_TEMPLATE = 'EOL_DATE={}'
-    _EOL_NOTIFICATION_TITLE = 'Final software update'
+    # Value within {} expected to be the month and year.
+    _EXPECTED_WARNING_TITLE = 'Updates end {}'
 
     def cleanup(self):
         self._save_extra_update_engine_logs()
@@ -27,19 +33,50 @@ class autoupdate_EOL(update_engine_test.UpdateEngineTest):
                                  (self._EXPECTED_EOL_DATE, result))
 
 
-    def _check_eol_notification(self):
+    def _check_eol_notification(self, eol_date):
         """Checks that we are showing an EOL notification to the user."""
-        # TODO(crbug.com/995889): Should have notification checks based on
-        # EOL date.
-        pass
+        epoch = datetime.datetime(1970,1,1)
+        expected_eol_date = (epoch + datetime.timedelta(eol_date))
+        expected_warning_begins_date = (expected_eol_date
+                                        - datetime.timedelta(
+                                          self._DAYS_BEFORE_EOL_START_WARNING))
 
+        expected_final_title = self._EXPECTED_FINAL_TITLE
+        expected_warning_title = (self._EXPECTED_WARNING_TITLE.
+            format(expected_eol_date.strftime("%B %Y")))
+
+        def find_notification(expected_title):
+            """Helper to find notification."""
+            notifications = cr.get_visible_notifications()
+            return any([n['title'] == expected_title
+                        for n in (notifications or [])])
+
+        with chrome.Chrome(autotest_ext=True, logged_in=True) as cr:
+            def check_eol_notifications():
+                """ Checks if correct notification is shown """
+                final_notification = find_notification(expected_final_title)
+                warning_notification = find_notification(expected_warning_title)
+
+                now = datetime.datetime.utcnow()
+                if expected_eol_date <= now:
+                    return final_notification and not warning_notification
+                elif expected_warning_begins_date <= now:
+                    return not final_notification and warning_notification
+                return not final_notification and not warning_notification
+
+            utils.poll_for_condition(
+                                condition=lambda: check_eol_notifications(),
+                                desc='End of Life Notification UI passed',
+                                timeout=5,
+                                sleep_interval=1)
 
     def run_once(self, eol_date):
         """
         The main test.
 
-        @param eol_date: the value passed along to NanoOmahaDevServer placed
-                         within the _eol_date tag in the Omaha response.
+        @param eol_date: the days from epoch value passed along to
+                         NanoOmahaDevServer placed within the _eol_date tag
+                         in the Omaha response.
         """
         # Override the expected values based on input of |eol_date| and params.
         self._EXPECTED_EOL_DATE = \
@@ -54,4 +91,4 @@ class autoupdate_EOL(update_engine_test.UpdateEngineTest):
                                wait_for_completion=True)
 
         self._check_eol_info()
-        self._check_eol_notification()
+        self._check_eol_notification(eol_date)
