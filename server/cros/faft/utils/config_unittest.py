01b80bb08ae62a2fa6c687a0d34bdf91af9068f4
Greg Edelston <gredelston@google.com>
2019-10-14 10:53:28 -0600

    Add base class to create/destroy mock config files
    I'm going to add a new unittest.TestCase to address b:142147850.
    In order to do so, I will want to create mock JSON config files.
    The pre-existing InheritanceTestCase already handled this nicely.
    
    This change moves the mockfile creation/deletion into an inherited base
    class so that I can reuse it for a different TestCase.
    
    TEST=config_unittest
    BUG=None
    
    Change-Id: Id8a7b00bcd3f86d04c06e130c9b81e4d25362b12
    Signed-off-by: Greg Edelston <gredelston@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/1859656
    Legacy-Commit-Queue: Commit Bot <commit-bot@chromium.org>
    Reviewed-by: Dana Goyette <dgoyette@chromium.org>

--- server/cros/faft/utils/config_unittest.py
+++ server/cros/faft/utils/config_unittest.py
@@ -20,38 +20,66 @@ class CanLoadDefaultTestCase(unittest.TestCase):
         self.assertIsInstance(cfg.has_keyboard, bool)
 
 
-class InheritanceTestCase(unittest.TestCase):
-    """Ensure that platforms inherit attributes correctly"""
+class _MockConfigTestCaseBaseClass(unittest.TestCase):
+    """
+    Base class which handles the setup/teardown of mock config files.
+
+    Sub-classes should declare a class attribute, mock_configs,
+    as a dict representing all platforms to be written as JSON files.
+    This class writes those JSON files during setUp() and deletes them
+    during tearDown().
+    During runTest(), sub-classes can create config.Config instances by name
+    and run assertions as normal.
+
+    """
+
+    mock_configs = None
 
     def setUp(self):
-        """Set up a tempfile containing test data"""
+        """Set up a tempfile containing the test data"""
+        if self.mock_configs is None:
+            return
         self.original_config_dir = config.CONFIG_DIR
         config.CONFIG_DIR = tempfile.mkdtemp()
-        mock_configs = {
-                'DEFAULTS': {
-                        'no_override': 'default',
-                        'parent_override': 'default',
-                        'child_override': 'default',
-                        'both_override': 'default',
-                        'parent': None
-                },
-                'childboard': {
-                        'child_override': 'child',
-                        'both_override': 'child',
-                        'parent': 'parentboard'
-                },
-                'parentboard': {
-                        'parent_override': 'parent',
-                        'both_override': 'parent'
-                }
-        }
         self.mock_config_files = []
-        for platform in mock_configs:
+        for platform in self.mock_configs:
             mock_config_file = os.path.join(config.CONFIG_DIR,
                                             '%s.json' % platform)
             with open(mock_config_file, 'w') as f:
-                json.dump(mock_configs[platform], f)
+                json.dump(self.mock_configs[platform], f)
             self.mock_config_files.append(mock_config_file)
+
+    def tearDown(self):
+        """After tests are complete, delete the tempfile"""
+        if self.mock_configs is None:
+            return
+        for tf in self.mock_config_files:
+            os.remove(tf)
+        os.rmdir(config.CONFIG_DIR)
+        config.CONFIG_DIR = self.original_config_dir
+
+
+class InheritanceTestCase(_MockConfigTestCaseBaseClass):
+    """Ensure that platforms inherit attributes correctly"""
+
+    mock_configs = {
+            'DEFAULTS': {
+                    'no_override': 'default',
+                    'parent_override': 'default',
+                    'child_override': 'default',
+                    'both_override': 'default',
+                    'parent': None
+            },
+            'childboard': {
+                    'child_override': 'child',
+                    'both_override': 'child',
+                    'parent': 'parentboard'
+            },
+            'parentboard': {
+                    'parent_override': 'parent',
+                    'both_override': 'parent'
+            }
+    }
 
     def runTest(self):
         """
@@ -61,6 +89,7 @@ class InheritanceTestCase(unittest.TestCase):
             A platform not found in the config file
         """
         child_config = config.Config('childboard')
+        #print(child_config)
         self.assertEqual(child_config.no_override, 'default')
         self.assertEqual(child_config.parent_override, 'parent')
         self.assertEqual(child_config.child_override, 'child')
@@ -81,14 +110,7 @@ class InheritanceTestCase(unittest.TestCase):
         self.assertEqual(foo_config.both_override, 'default')
 
         # While we're here, verify that str(config) doesn't break
-        str(child_config)  # pylint: distable=pointless-statement
-
-    def tearDown(self):
-        """After tests are complete, delete the tempfile"""
-        for tf in self.mock_config_files:
-            os.remove(tf)
-        os.rmdir(config.CONFIG_DIR)
-        config.CONFIG_DIR = self.original_config_dir
+        str(child_config)  # pylint: disable=pointless-statement
 
 
 class PlatformNamesTestCase(unittest.TestCase):
