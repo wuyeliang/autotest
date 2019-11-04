e9f35007037ca27ef55047e75d3fd42107df5adb
Po-Hsien Wang <pwang@chromium.org>
2019-11-04 19:47:32 +0000

    Revert "utils: fix typo in hwmon"
    This reverts commit b0fcff9f7597dba93f729c7652a62f1948de90c1.
    
    Reason for revert: Break glaquarium
    
    Original change's description:
    > utils: fix typo in hwmon
    > 
    > This is the reland of CL:1867752.
    > Not caching the paths probably caused minor performance issues
    > in perf tests.
    > 
    > BUG=chromium:1016189
    > TEST=None
    > 
    > Change-Id: I5bd87e4879d8796e851f000152b9a68dc4b29d10
    > Reviewed-on: https://chromium-review.googlesource.com/1872819
    > Tested-by: Po-Hsien Wang <pwang@chromium.org>
    > Commit-Ready: ChromeOS CL Exonerator Bot <chromiumos-cl-exonerator@appspot.gserviceaccount.com>
    > Legacy-Commit-Queue: Commit Bot <commit-bot@chromium.org>
    > Reviewed-by: Stéphane Marchesin <marcheu@chromium.org>
    > Reviewed-by: Ilja H. Friedel <ihf@chromium.org>
    
    Bug: chromium:1016189
    Change-Id: I8a2b45ee06430685395173fcd1275f9c3ae04a49
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/autotest/+/1898275
    Reviewed-by: Po-Hsien Wang <pwang@chromium.org>
    Commit-Queue: Po-Hsien Wang <pwang@chromium.org>
    Tested-by: Po-Hsien Wang <pwang@chromium.org>

--- client/bin/utils.py
+++ client/bin/utils.py
@@ -1863,11 +1863,10 @@ def _get_hwmon_paths(file_pattern):
     #    /sys/class/hwmon/hwmon*/
     #    /sys/devices/virtual/hwmon/hwmon*/
     #    /sys/devices/platform/coretemp.0/
-    global _hwmon_paths
     if not _hwmon_paths:
         cmd = 'find /sys/ -name "' + file_pattern + '"'
-        _hwmon_paths = utils.run(cmd, verbose=False).stdout.splitlines()
-    return _hwmon_paths
+        _hwon_paths = utils.run(cmd, verbose=False).stdout.splitlines()
+    return _hwon_paths
 
 
 def get_temperature_critical():
