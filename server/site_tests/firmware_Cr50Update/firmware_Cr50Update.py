7392d4a1baf6ffd097cda8182b164dfae8017c3b
Mary Ruthven <mruthven@chromium.org>
2019-11-15 14:56:28 -0800

    firmware_Cr50Update: don't eraseflashinfo
    We don't use this test for verifying anything that requires
    eraseflashinfo. Other tests verify board id. Remove the chip board id
    stuff from this test and just stick to verifying update.
    
    BUG=b:144502375
    TEST=run firmware_Cr50Update
    
    Change-Id: I3243b1b3175521954557b3dceddbbf2702da2715
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/autotest/+/1920181
    Tested-by: Mary Ruthven <mruthven@chromium.org>
    Commit-Queue: Mary Ruthven <mruthven@chromium.org>
    Reviewed-by: Dana Goyette <dgoyette@chromium.org>

--- server/site_tests/firmware_Cr50Update/firmware_Cr50Update.py
+++ server/site_tests/firmware_Cr50Update/firmware_Cr50Update.py
@@ -70,18 +70,6 @@ class firmware_Cr50Update(Cr50Test):
         self.verify_update_order()
         logging.info("Update %s", self.update_order)
 
-        self.chip_bid = None
-        self.chip_flags = None
-        chip_bid_info = cr50_utils.GetChipBoardId(self.host)
-        if chip_bid_info != cr50_utils.ERASED_CHIP_BID:
-            self.chip_bid, _, self.chip_flags = chip_bid_info
-            logging.info('chip board id will be erased during rollback. %x:%x '
-                'will be restored after rollback.',  self.chip_bid,
-                self.chip_flags)
-        else:
-            logging.info('No chip board id is set. This test will not attempt '
-                'to restore anything during rollback.')
-
         self.device_update_path = cr50_utils.GetActiveCr50ImagePath(self.host)
         # Update to the dev image
         self.run_update(self.DEV_NAME)
@@ -122,8 +110,7 @@ class firmware_Cr50Update(Cr50Test):
         # If a rollback is needed, flash the image into the inactive partition,
         # on or use usb_update to update to the new image if it is requested.
         if use_usb_update or rollback:
-            self.cr50_update(image_path, rollback=rollback,
-                chip_bid=self.chip_bid, chip_flags=self.chip_flags)
+            self.cr50_update(image_path, rollback=rollback)
             self.check_state((self.checkers.crossystem_checker,
                               {'mainfw_type': 'normal'}))
 
