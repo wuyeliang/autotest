974d2baf6ad77cc6144e44c6c7b370b13418b896
Greg Edelston <gredelston@google.com>
2019-11-20 10:46:06 -0700

    Remove mosys platform model from GetModelName RPC
    BUG=chromium:1023647
    TEST=test_that firmware_FAFTSetup, on both a unibuild board and a
    non-unibuild board, with an R79+ image, with an extra logging line:
    logging.info(faft_client.System.GetModelName())
    
    Cq-Depend: chromium:1925886
    Change-Id: I21bb51d7ebdb64be85ad8edbae83ed6497d39608
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/autotest/+/1925561
    Tested-by: Greg Edelston <gredelston@google.com>
    Reviewed-by: Kevin Shelton <kmshelton@chromium.org>
    Commit-Queue: Greg Edelston <gredelston@google.com>

--- client/cros/faft/rpc_functions.py
+++ client/cros/faft/rpc_functions.py
@@ -765,11 +765,12 @@ class SystemServicer(object):
 
         @return: A string of the model name.
         """
-        lines = self._os_if.run_shell_command_get_output(
-                '(mosys -vvv platform model 2>&1) || echo Failed')
+        cmd = 'cros_config / name || echo Failed'
+        lines = self._os_if.run_shell_command_get_output(cmd)
         if lines[-1].strip() == 'Failed':
-            raise Exception('Failed getting model name: ' + '\n'.join(lines))
-        return lines[-1].strip()
+            raise Exception('Failed getting platform name: ' +
+                            '\n'.join(lines))
+        return lines[-1]
 
     def DevTpmPresent(self):
         """Check if /dev/tpm0 is present.
