9490f81ded7eaa33f2ac910dc4703a69c0401051
David Haddock <dhaddock@chromium.org>
2020-02-10 14:23:16 -0800

    autoupdate_Rollback: Update to use update_engine_test.
    This is the last AU test we monitor that has not been migrated to use
    the same base class as the rest.
    
    BUG=chromium:1048916
    TEST=autoupdate_Rollback, autoupdate_Rollback_powerwash_before_rollback.
    
    Change-Id: I879d624e3189762a67cc87c76ba7375de222b653
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/autotest/+/2048188
    Tested-by: David Haddock <dhaddock@chromium.org>
    Reviewed-by: Amin Hassani <ahassani@chromium.org>
    Commit-Queue: David Haddock <dhaddock@chromium.org>

--- server/site_tests/autoupdate_Rollback/autoupdate_Rollback.py
+++ server/site_tests/autoupdate_Rollback/autoupdate_Rollback.py
@@ -6,30 +6,26 @@ import logging
 import os
 
 from autotest_lib.client.common_lib import error
-from autotest_lib.server import test
-from autotest_lib.server.cros import autoupdate_utils
+from autotest_lib.server.cros import autoupdater
+from autotest_lib.server.cros.update_engine import update_engine_test
 
 POWERWASH_COMMAND = 'safe fast keepimg'
 POWERWASH_MARKER_FILE = '/mnt/stateful_partition/factory_install_reset'
 STATEFUL_MARKER_FILE = '/mnt/stateful_partition/autoupdate_Rollback_flag'
 
-class autoupdate_Rollback(test.test):
+class autoupdate_Rollback(update_engine_test.UpdateEngineTest):
     """Test that updates the machine and performs rollback."""
     version = 1
 
-    def _powerwash(self, host):
-        """
-        Powerwashes DUT.
-
-        @param host: The DUT we are testing on.
-
-        """
+    def _powerwash(self):
+        """Powerwashes DUT."""
         logging.info('Powerwashing device before rollback.')
-        host.run('echo car > %s' % STATEFUL_MARKER_FILE)
-        host.run("echo '%s' > %s" % (POWERWASH_COMMAND, POWERWASH_MARKER_FILE))
-        host.reboot()
-        marker = host.run('[ -e %s ]' % STATEFUL_MARKER_FILE,
-                          ignore_status=True, ignore_timeout=True)
+        self._host.run('echo car > %s' % STATEFUL_MARKER_FILE)
+        self._host.run("echo '%s' > %s" % (POWERWASH_COMMAND,
+                                           POWERWASH_MARKER_FILE))
+        self._host.reboot()
+        marker = self._host.run('[ -e %s ]' % STATEFUL_MARKER_FILE,
+                                ignore_status=True, ignore_timeout=True)
         if marker is None or marker.exit_status == 0:
             raise error.TestFail("Powerwash cycle didn't remove the marker "
                                  "file on the stateful partition.")
@@ -46,28 +42,27 @@ class autoupdate_Rollback(test.test):
         # Rollback operation.
         preserved_prefs_path = ('/mnt/stateful_partition/unencrypted/preserve'
                                 '/update_engine/prefs/')
-        self.host.run('rm %s %s' %
+        self._host.run('rm %s %s' %
                       (os.path.join(preserved_prefs_path, 'rollback-version'),
                        os.path.join(preserved_prefs_path, 'rollback-happened')),
                       ignore_status=True)
         # Restart update-engine to pick up new prefs.
-        self.host.run('restart update-engine', ignore_status=True)
+        self._host.run('restart update-engine', ignore_status=True)
 
 
-    def run_once(self, host, job_repo_url=None,
-                 powerwash_before_rollback=False):
+    def run_once(self, job_repo_url=None, powerwash_before_rollback=False):
         """Runs the test.
 
-        @param host: A host object representing the DUT.
         @param job_repo_url: URL to get the image.
+        @param powerwash_before_rollback: True if we should rollback before
+                                          powerwashing.
 
         @raise error.TestError if anything went wrong with setting up the test;
                error.TestFail if any part of the test has failed.
 
         """
-        self.host = host
-
-        updater = autoupdate_utils.get_updater_from_repo_url(host, job_repo_url)
+        update_url = self.get_update_url_for_test(job_repo_url)
+        updater = autoupdater.ChromiumOSUpdater(update_url, self._host)
 
         initial_kernel, updated_kernel = updater.get_kernel_state()
         logging.info('Initial device state: active kernel %s, '
@@ -75,19 +70,19 @@ class autoupdate_Rollback(test.test):
 
         logging.info('Performing an update.')
         updater.update_image()
-        host.reboot()
+        self._host.reboot()
 
         # We should be booting from the new partition.
         error_message = 'Failed to set up test by updating DUT.'
         updater.verify_boot_expectations(updated_kernel, error_message)
 
         if powerwash_before_rollback:
-            self._powerwash(host)
+            self._powerwash()
 
         logging.info('Update verified, initiating rollback.')
         # Powerwash is tested separately from rollback.
         updater.rollback_rootfs(powerwash=False)
-        host.reboot()
+        self._host.reboot()
 
         # We should be back on our initial partition.
         error_message = ('Autoupdate reported that rollback succeeded but we '
