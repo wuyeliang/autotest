wuyeliang/autotest

commit f9d5ba2441d8d383c47b9476b91040b2ef4e97f5

power_status: platform-agnostic wraparound detection for RC6 residency
Do not assume the rc6_residency_ms of sysfs is monotonically increasing,
instead, scarify the precision of residency polling to avoid negative
residency duration.

To archive this, cache a previous RC6 residency to detect wraparound,
apply delta to current residency then cache it to an accumulated RC6
residency since the object created; therefore, we make the accumulated
RC6 in monotonically increasing in userspace.

In theory, the worst-case affecting RC6 portion per loop is the polling
period divide by loop period; assuming PLT in 20 seconds polling per one
hour loop, it affects RC6 ratio about -0.005 (-0.5%) in a worst-case.

BUG=chromium:1033843, b:145623577
TEST=do long PLT, at least two hours for big-core, one for small-core,
examine report, focus on RC0/RC6, make sure the metric has no negative
residency which would affect the diagram of power_log.html, instead by
wraparound detection warning in the autotest debug logs.

Change-Id: I1e3f3abc8fa7fb4661ed650cc8bd665f87da5e12
Signed-off-by: Harry Pan <harry.pan@intel.com>
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/autotest/+/1978046
Commit-Queue: Puthikorn Voravootivat <puthik@chromium.org>
Reviewed-by: Puthikorn Voravootivat <puthik@chromium.org>
Tested-by: Puthikorn Voravootivat <puthik@chromium.org>

client/cros/power/power_status.py
@@ -2455,7 +2455,8 @@ class RC6ResidencyStats(object):
     """
     def __init__(self):
         self._rc6_enable_checked = False
-        self._initial_stat = self._parse_rc6_residency_info()
+        self._previous_stat = self._parse_rc6_residency_info()
+        self._accumulated_stat = 0
 
     def get_accumulated_residency_msecs(self):
         """Check number of RC6 state entry since the class has been initialized.
@@ -2463,7 +2464,27 @@ class RC6ResidencyStats(object):
         @returns int of RC6 residency in milliseconds since instantiation.
         """
         current_stat = self._parse_rc6_residency_info()
-        return (current_stat - self._initial_stat)
+
+        # The problem here is that we cannot assume the rc6_residency_ms is
+        # monotonically increasing by current kernel i915 implementation.
+        #
+        # Considering different hardware has different wraparound period,
+        # this is a mitigation plan to deal with different wraparound period
+        # on various platforms, in order to make the test platform agnostic.
+        #
+        # This scarifes the accuracy of RC6 residency a bit, up on the calling
+        # period.
+        #
+        # Reference: Bug 94852 - [SKL] rc6_residency_ms unreliable
+        # (https://bugs.freedesktop.org/show_bug.cgi?id=94852)
+        if current_stat < self._previous_stat:
+          logging.warning('GPU: Detect rc6_residency_ms wraparound')
+          self._accumulated_stat += current_stat
+        else:
+          self._accumulated_stat += current_stat - self._previous_stat
+
+        self._previous_stat = current_stat
+        return self._accumulated_stat
 
     def _is_rc6_enable(self):
         """
@@ -2689,7 +2710,6 @@ class GPURC6Stats(AbstractStats):
 
     def _read_stats(self):
         total = int(time.time() * 1000)
-        # TODO (harry.pan): Fix wraparound case.
         msecs = self._rc6.get_accumulated_residency_msecs()
         stats = collections.defaultdict(int)
         stats['RC6'] += msecs

commit 104e0918ceb7088f00e1a3d34787271b7694c9e4

power_status: improve the accuracy of RC6 residency for wraparound case
Suggested by Puthikorn, for the modern processors with a known overflow
count, apply a constant of max counter to maintain the accuracy.

Note that we patch this for devices running with an older kernel prior
than v4.17, for those with a newer kernel, the graphic driver has wraparound
dealing; Here we do not assume the case of force kernel downgrade.

Fixes: f9d5ba2441d8 ("power_status: platform-agnostic wraparound detection for RC6 residency")

BUG=chromium:1033843, b:145623577
TEST=do long PLT, at least two hours for big-core, one for small-core,
focus on RC0/RC6, manually add hacking debug print into the debug log,
validate the counter dealing for wraparound case.
Alternative validation is to dump rc6_residency_ms frequently and manually
examine the max count on given machines of the uarch list.
i.e.,
while true; do cat /sys/class/drm/card0/power/rc6_residency_ms; sleep 0.5; done"

Change-Id: I2bcc730c9458e7fac9ef8038b0bb1b296c3b41b9
Signed-off-by: Harry Pan <harry.pan@intel.com>
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/autotest/+/2008909
Commit-Queue: Puthikorn Voravootivat <puthik@chromium.org>
Tested-by: Puthikorn Voravootivat <puthik@chromium.org>
Reviewed-by: Puthikorn Voravootivat <puthik@chromium.org>

client/cros/power/power_status.py
@@ -2472,6 +2472,22 @@ class RC6ResidencyStats(object):
         self._previous_stat = self._parse_rc6_residency_info()
         self._accumulated_stat = 0
 
+        # Setup max RC6 residency count for modern chips. The table order
+        # is in small/big-core first, follows by the uarch name. We don't
+        # need to deal with the wraparound for devices with v4.17+ kernel
+        # which has the commit 817cc0791823 ("drm/i915: Handle RC6 counter wrap").
+        cpu_uarch = utils.get_intel_cpu_uarch()
+        self._max_counter = {
+          # Small-core w/ GEN9 LP graphics
+          'Airmont':      3579125,
+          'Goldmont':     3579125,
+          # Big-core
+          'Broadwell':    5497558,
+          'Haswell':      5497558,
+          'Kaby Lake':    5497558,
+          'Skylake':      5497558,
+        }.get(cpu_uarch, None)
+
     def get_accumulated_residency_msecs(self):
         """Check number of RC6 state entry since the class has been initialized.
 
@@ -2491,9 +2507,18 @@ class RC6ResidencyStats(object):
         #
         # Reference: Bug 94852 - [SKL] rc6_residency_ms unreliable
         # (https://bugs.freedesktop.org/show_bug.cgi?id=94852)
+        #
+        # However for modern processors with a known overflow count, apply
+        # constant of RC6 max counter to improve accuracy.
+        #
+        # Note that the max counter is bound for sysfs overflow, while the
+        # accumulated residency here is the diff against the first reading.
         if current_stat < self._previous_stat:
-          logging.warning('GPU: Detect rc6_residency_ms wraparound')
-          self._accumulated_stat += current_stat
+          if self._max_counter is None:
+            logging.warning('GPU: Detect rc6_residency_ms wraparound')
+            self._accumulated_stat += current_stat
+          else:
+            self._accumulated_stat += current_stat + (self._max_counter - self._previous_stat)
         else:
           self._accumulated_stat += current_stat - self._previous_stat
 