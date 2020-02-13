124741394ff56bd113a83fa858ecba800589eca5
Alexis Savery <asavery@chromium.org>
2020-02-13 14:16:13 -0800

    hardware_StorageWearoutDetect: add NVMe support, update SATA fail
    Add the ability to detect an NVMe device. Since storage_info.txt will
    also have Model Number: for NVMe, change SSD_DETECT to SATA_DETECT and
    look for "SATA Version" instead.
    
    Detect failing NVMe based on the Percentage Used value, and add SATA_FAIL
    to use the Percentage Used Endurance Indicator.
    
    BUG=chromium:1048258
    TEST=Ran hardware_StorageWearoutDetect on SATA, eMMC and NVMe devices. Created
    example storage_info.txt files to use to test different values.
    
    Change-Id: I38e98b54817f97eda6f1986d3288a1847a1df288
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/autotest/+/2055352
    Reviewed-by: Gwendal Grignou <gwendal@chromium.org>
    Tested-by: Alexis Savery <asavery@chromium.org>
    Commit-Queue: Alexis Savery <asavery@chromium.org>

--- client/site_tests/hardware_StorageWearoutDetect/hardware_StorageWearoutDetect.py
+++ client/site_tests/hardware_StorageWearoutDetect/hardware_StorageWearoutDetect.py
@@ -16,6 +16,8 @@ class hardware_StorageWearoutDetect(test.test):
     The test will be failed if:
     - At least one SMART variable has value under its threshold
       or
+    - Percentage Used reported by SMART for NVMe or SATA is above 90
+      or
     - eMMC wear out status variable is in 90-100% band or higher (
       DEVICE_LIFE_TIME_EST_TYP_A). Seeing this consistently means the lab
       device may have to be replaced.
@@ -25,11 +27,14 @@ class hardware_StorageWearoutDetect(test.test):
     STORAGE_INFO_PATH = '/var/log/storage_info.txt'
     STORAGE_INFO_COMMON_PATH = '/usr/share/misc/storage-info-common.sh'
 
-    # Example     "   Model Number:    LITEONIT LSS-32L6G-HP"
-    SSD_DETECT = r"\s*Model Number:\s*(?P<model>.*)\s*$"
+    # Example     "SATA Version is: SATA 3.1, 6.0 Gb/s (current: 6.0 Gb/s)"
+    SATA_DETECT = r"SATA Version is:.*"
 
     # Example     "   Extended CSD rev 1.7 (MMC 5.0)"
     MMC_DETECT = r"\s*Extended CSD rev.*MMC (?P<version>\d+.\d+)"
+
+    # Example     "SMART/Health Information (NVMe Log 0x02, NSID 0xffffffff)"
+    NVME_DETECT = r".*NVMe Log .*"
 
     # Field meaning and example line that have failing attribute
     # ID# ATTRIBUTE_NAME          FLAGS    VALUE WORST THRESH FAIL RAW_VALUE
@@ -39,10 +44,15 @@ class hardware_StorageWearoutDetect(test.test):
                    (\s+\d{3}){3}               # three 3-digits numbers
                    \s+NOW                      # fail indicator"""
 
+    # Ex "0x07  0x008  1          100  ---  Percentage Used Endurance Indicator"
+    SATA_FAIL = r".*\s{3,}(?P<param>(99|\d{3}))\s+.*Percentage Used Endurance.*"
+
     # Ex "Device life time estimation type A [DEVICE_LIFE_TIME_EST_TYP_A: 0x01]"
     # 0x0a means 90-100% band, 0x0b means over 100% band -> find not digit
-    MMC_FAIL = r".*(?P<param>DEVICE_LIFE_TIME_EST_TYP_.): 0x0\D"
+    MMC_FAIL = r".*(?P<param>DEVICE_LIFE_TIME_EST_TYP_.)]?: 0x0\D"
 
+    # Ex "Percentage Used:         100%"
+    NVME_FAIL = r"Percentage Used:\s+(?P<param>(99|\d{3}))%"
 
     def run_once(self, use_cached_result=True):
         """
@@ -69,17 +79,17 @@ class hardware_StorageWearoutDetect(test.test):
             raise error.TestFail(msg)
 
         mmc_detect = False
-        ssd_detect = False
+        sata_detect = False
         legacy_mmc = False
+        nvme_detect = False
         fail_msg = ''
 
         with open(self.STORAGE_INFO_PATH) as f:
             for line in f:
-                m = re.match(self.SSD_DETECT, line)
+                m = re.match(self.SATA_DETECT, line)
                 if m:
-                    model = m.group('model')
-                    ssd_detect = True
-                    logging.info('Found SSD model %s', model)
+                    sata_detect = True
+                    logging.info('Found SATA device')
 
                 m = re.match(self.MMC_DETECT, line)
                 if m:
@@ -88,6 +98,11 @@ class hardware_StorageWearoutDetect(test.test):
                         legacy_mmc = True
                     mmc_detect = True
                     logging.info('Found eMMC version %s', version)
+
+                m = re.match(self.NVME_DETECT, line)
+                if m:
+                    nvme_detect = True
+                    logging.info('Found NVMe device')
 
                 m = re.match(self.SSD_FAIL, line, re.X)
                 if m:
@@ -99,7 +114,17 @@ class hardware_StorageWearoutDetect(test.test):
                     param = m.group('param')
                     fail_msg += 'MMC failure ' + param
 
-        if not ssd_detect and not mmc_detect:
+                m = re.match(self.SATA_FAIL, line)
+                if m:
+                    param = m.group('param')
+                    fail_msg += 'SATA failure, percentage used ' + param
+
+                m = re.match(self.NVME_FAIL, line)
+                if m:
+                    param = m.group('param')
+                    fail_msg += 'NVMe failure, percentage used ' + param
+
+        if not sata_detect and not mmc_detect and not nvme_detect:
             raise error.TestFail('Can not detect storage device.')
 
         if fail_msg:
