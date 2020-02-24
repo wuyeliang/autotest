0640eddd87d5e81d73fb9b78171f25c9b26175eb
Keith Haddow <haddowk@chromium.org>
2020-02-23 18:28:56 -0800

    [autotest] Uprev CTS Instant version to 9.0_r11
    BUG=b:68778082
    TEST=None
    
    Change-Id: Iceea6ac45ab5ef721e8d91d6d527425a1ea5e2bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/autotest/+/2068231
    Reviewed-by: Kazuhiro Inaba <kinaba@chromium.org>
    Commit-Queue: Kazuhiro Inaba <kinaba@chromium.org>
    Tested-by: Kazuhiro Inaba <kinaba@chromium.org>
    Auto-Submit: Kazuhiro Inaba <kinaba@chromium.org>
    (cherry picked from commit d3f769e4d981cdccd802e68e25f9adcee362570b)
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/autotest/+/2108434

--- server/site_tests/cheets_CTS_Instant/cheets_CTS_Instant.py
+++ server/site_tests/cheets_CTS_Instant/cheets_CTS_Instant.py
@@ -25,8 +25,8 @@ _CTS_TIMEOUT_SECONDS = 3600
 _PUBLIC_CTS = 'https://dl.google.com/dl/android/cts/'
 _PARTNER_CTS = 'gs://chromeos-partner-cts/'
 _CTS_URI = {
-    'arm': _PUBLIC_CTS + 'android-cts_instant-9.0_r10-linux_x86-arm.zip',
-    'x86': _PUBLIC_CTS + 'android-cts_instant-9.0_r10-linux_x86-x86.zip',
+    'arm': _PUBLIC_CTS + 'android-cts_instant-9.0_r11-linux_x86-arm.zip',
+    'x86': _PUBLIC_CTS + 'android-cts_instant-9.0_r11-linux_x86-x86.zip',
 }
 _CTS_MEDIA_URI = _PUBLIC_CTS + 'android-cts-media-1.4.zip'
 _CTS_MEDIA_LOCALPATH = '/tmp/android-cts-media'
