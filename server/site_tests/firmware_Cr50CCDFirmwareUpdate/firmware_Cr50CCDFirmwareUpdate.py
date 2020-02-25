c27f4b43e64156ce3c236a298ffacdc9f11f682a
Namyoon Woo <namyoon@chromium.org>
2020-02-25 14:11:17 -0800

    autotest: introducing firmware_Cr50CCDFirmwareUpdate
    This test is to test flashing EC and AP firmware in CCD mode.
    It downloads the latest image from the server, flashes it
    on EC and AP, and verifies the version number.
    
    BUG=b:123717818
    TEST=ran on Octopus Fleex.
    $ sudo servod -b octopus --allow-dual-v4
    
    $ test_that -b fleex ${IP} --autotest_dir ${TESTDIR}
    firmware_Cr50CCDFirmwareUpdate.faft_cr50_tot
    
    Change-Id: I8dcbf3bd52eb5d0c9b83ee5fa160653046e8bb39
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/autotest/+/2072873
    Tested-by: Namyoon Woo <namyoon@chromium.org>
    Commit-Queue: Namyoon Woo <namyoon@chromium.org>
    Reviewed-by: Mary Ruthven <mruthven@chromium.org>

---
+++ server/site_tests/firmware_Cr50CCDFirmwareUpdate/firmware_Cr50CCDFirmwareUpdate.py
@@ -0,0 +1,56 @@
+# Copyright 2020 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+
+"""The autotest performing FW update, both EC and AP in CCD mode."""
+
+
+from autotest_lib.client.common_lib import error
+from autotest_lib.server.cros.faft.cr50_test import Cr50Test
+
+class firmware_Cr50CCDFirmwareUpdate(Cr50Test):
+    """A test that can provision a machine to the correct firmware version."""
+
+    version = 1
+
+    def initialize(self, host, cmdline_args, full_args):
+        """Initialize the test and check if cr50 exists.
+
+        Raises:
+            TestNAError: If the dut is not proper for this test for its RDD
+                         recognition problem.
+        """
+        super(firmware_Cr50CCDFirmwareUpdate,
+              self).initialize(host, cmdline_args, full_args)
+
+        servo_type = self.servo.get_servo_version()
+        if 'ccd_cr50' not in servo_type:
+            raise error.TestNAError('unsupported servo type: %s' % servo_type)
+
+    def run_once(self, host, rw_only=False):
+        """The method called by the control file to start the test.
+
+        Args:
+          host:  a CrosHost object of the machine to update.
+          rw_only: True to only update the RW firmware.
+
+        Raises:
+          TestFail: if the firmware version remains unchanged.
+          TestNAError: if the test environment is not properly set.
+                       e.g. the servo type doesn't support this test.
+        """
+
+        # If build info was not given and explicitly it was requested to
+        # get the release version from image archive search, then
+        # do it so.
+        board = self.servo.get_board()
+        value = host.get_latest_release_version(board)
+
+        # Fast open cr50 and check if testlab is enabled.
+        self.fast_open(enable_testlab=True)
+        if self.servo.has_control('active_v4_device'):
+            self.servo.set('active_v4_device', 'ccd_cr50')
+
+        host.firmware_install(build=value, rw_only=rw_only,
+                              dest=self.resultsdir, verify_version=True)
