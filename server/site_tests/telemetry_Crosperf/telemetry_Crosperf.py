6818aa4720ef87281c96cc02434ae037fb640ce0
Zhizhou Yang <zhizhouy@google.com>
2020-03-02 11:55:59 -0800

    telemetry_Crosperf: adding --legacy-json-trace-format flag
    This patch adds --legacy-json-trace-format flag to fix test failures in
    certain telemetry benchmarks.
    
    BUG=chromium:1055528
    TEST=Passed loading.desktop
    
    Change-Id: I64015e22f3c4e5597984196461616cd9190db806
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/autotest/+/2083577
    Commit-Queue: Zhizhou Yang <zhizhouy@google.com>
    Tested-by: Zhizhou Yang <zhizhouy@google.com>
    Auto-Submit: Zhizhou Yang <zhizhouy@google.com>
    Reviewed-by: George Burgess <gbiv@chromium.org>

--- server/site_tests/telemetry_Crosperf/telemetry_Crosperf.py
+++ server/site_tests/telemetry_Crosperf/telemetry_Crosperf.py
@@ -314,7 +314,10 @@ class telemetry_Crosperf(test.test):
                 run_turbostat(dut, TURBOSTAT_LOG) as _turbo_cm, \
                 run_top(dut, TOP_LOG, top_interval) as _top_cm:
 
-                arguments = []
+                arguments = [
+                    # Adding this option for http://crbug.com/1055528.
+                    '--legacy-json-trace-format',
+                ]
                 if test_args:
                     arguments.extend(shlex.split(test_args))
                 if profiler_args:
