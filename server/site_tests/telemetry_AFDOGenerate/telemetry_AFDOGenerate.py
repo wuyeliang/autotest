916ac1a1b84d3fd7fffb239767c9db5ffa6c6ba7
Chinglin Yu <chinglinyu@chromium.org>
2020-03-03 18:36:00 +0800

    autotest: remove --legacy-json-trace-format flag in running telemetry.
    Remove the option of running telemetry with legacy JSON trace format
    since the trace_process now fetches the trace_processor_shell binary
    from cloud as a fallback.
    
    BUG=chromium:1047040
    TEST=Tested on DUT and checked that trace_processor_shell is downloaded
    on DUT correctly.
    
    Change-Id: Ifc5c36578d6beff17bbd52a58d0c18e29c3d6054
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/autotest/+/2082230
    Reviewed-by: Luis Lozano <llozano@chromium.org>
    Tested-by: Chinglin Yu <chinglinyu@chromium.org>
    Tested-by: Zhizhou Yang <zhizhouy@google.com>
    Commit-Queue: Zhizhou Yang <zhizhouy@google.com>

--- server/site_tests/telemetry_AFDOGenerate/telemetry_AFDOGenerate.py
+++ server/site_tests/telemetry_AFDOGenerate/telemetry_AFDOGenerate.py
@@ -79,18 +79,15 @@ def _wait_for_process(host, pid, timeout=-1):
 TELEMETRY_AFDO_BENCHMARKS = (
         # page_cycler tests are deprecated. Replace them with loading.desktop.
         ('loading.desktop', ('--pageset-repeat=1',
-                             '--story-tag-filter=typical',
-                             '--legacy-json-trace-format')),
+                             '--story-tag-filter=typical')),
         ('loading.desktop', ('--pageset-repeat=1',
-                             '--story-tag-filter=intl_ja_zh',
-                             '--legacy-json-trace-format')),
+                             '--story-tag-filter=intl_ja_zh')),
         ('rendering.desktop',
          ('--story-tag-filter=tough_canvas',
-          '--story-filter="bouncing\\*\\|canvas\\*\\|microsoft\\*"',
-          '--legacy-json-trace-format')),
-        ('octane', ('--legacy-json-trace-format',)),
-        ('kraken', ('--legacy-json-trace-format',)),
-        ('speedometer2', ('--legacy-json-trace-format',)),
+          '--story-filter="bouncing\\*\\|canvas\\*\\|microsoft\\*"')),
+        ('octane', ),
+        ('kraken', ),
+        ('speedometer2', ),
 )
 
 # Temporarily disable this benchmark because it is failing a
