0a92765629830f4c2e944d75daaeb5204c0dcfb1
Abhishek Pandit-Subedi <abhishekpandit@chromium.org>
2020-03-16 10:27:26 -0700

    bluetooth: test_discover_by_device should poll
    The test_discovery_by_device triggers an active scan and can be flaky if
    the scan isn't performed long enough or the peer's advertising interval
    is low. Make the test poll discovery for 60s instead, which is the same
    behavior the controller has when looking for the peer.
    
    BUG=b:151634876
    TEST=Ran bluetooth_AdapterSRSanity
    
    Change-Id: I31b0aa0a9ccb41363ff75237159526d455d6decb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/autotest/+/2105917
    Tested-by: Abhishek Pandit-Subedi <abhishekpandit@chromium.org>
    Reviewed-by: Daniel Winkler <danielwinkler@google.com>
    Commit-Queue: Abhishek Pandit-Subedi <abhishekpandit@chromium.org>
    Auto-Submit: Abhishek Pandit-Subedi <abhishekpandit@chromium.org>

--- server/cros/bluetooth/bluetooth_adapter_tests.py
+++ server/cros/bluetooth/bluetooth_adapter_tests.py
@@ -1462,13 +1462,7 @@ class BluetoothAdapterTests(test.test):
         return has_device_initially or device_discovered
 
     def _test_discover_by_device(self, device):
-        device_discovered = device.Discover(self.bluetooth_facade.address)
-
-        self.results = {
-                'device_discovered': device_discovered
-        }
-
-        return all(self.results.values())
+        return device.Discover(self.bluetooth_facade.address)
 
     @test_retry_and_log(False)
     def test_discover_by_device(self, device):
@@ -1478,7 +1472,34 @@ class BluetoothAdapterTests(test.test):
 
         @returns: True if the adapter is found by the device.
         """
-        return self._test_discover_by_device(device)
+        adapter_discovered = False
+        discover_by_device = self._test_discover_by_device
+        discovered_initially = discover_by_device(device)
+
+        if not discovered_initially:
+            try:
+                utils.poll_for_condition(
+                        condition=(lambda: discover_by_device(device)),
+                        timeout=self.ADAPTER_DISCOVER_TIMEOUT_SECS,
+                        sleep_interval=
+                        self.ADAPTER_DISCOVER_POLLING_SLEEP_SECS,
+                        desc='Waiting for adapter to be discovered')
+                adapter_discovered = True
+            except utils.TimeoutError as e:
+                logging.error('test_discover_by_device: %s', e)
+            except Exception as e:
+                logging.error('test_discover_by_device: %s', e)
+                err = ('bluetoothd probably crashed.'
+                       'Check out /var/log/messages')
+                logging.error(err)
+            except:
+                logging.error('test_discover_by_device: unexpected error')
+
+        self.results = {
+            'adapter_discovered_initially': discovered_initially,
+            'adapter_discovered': adapter_discovered
+        }
+        return any(self.results.values())
 
     @test_retry_and_log(False)
     def test_discover_by_device_fails(self, device):
@@ -1488,7 +1509,10 @@ class BluetoothAdapterTests(test.test):
 
         @returns False if the adapter is found by the device.
         """
-        return not self._test_discover_by_device(device)
+        self.results = {
+                'adapter_discovered': self._test_discover_by_device(device)
+        }
+        return not any(self.results.values())
 
     @test_retry_and_log(False)
     def test_device_set_discoverable(self, device, discoverable):
